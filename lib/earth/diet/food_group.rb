7d9cd0b6a2f95ce0279fd91e11576e5e0e86e401
Derek Kastner <dkastner@gmail.com>
2010-07-23 13:42:54 -0400

    Added in names scope

--- lib/earth/diet/food_group.rb
+++ lib/earth/diet/food_group.rb
@@ -1,5 +1,15 @@
 class FoodGroup < ActiveRecord::Base
   set_primary_key :name
+
+  class << self
+    def names
+      connection.select_values arel_table.project(:name).to_sql
+    end
+    
+    def [](name)
+      find_by_name name.to_s
+    end
+  end
   
   data_miner do
     tap "Brighter Planet's food group data", Earth.taps_server
