a6057dadac8e7224475807e92dac503ae1f3e18e
Ian Hough <ijhough@gmail.com>
2010-07-28 12:51:10 -0700

    Fixing associations

--- lib/earth/industry/merchant_category.rb
+++ lib/earth/industry/merchant_category.rb
@@ -1,6 +1,8 @@
 class MerchantCategory < ActiveRecord::Base
   set_primary_key :mcc
-  
+
+  has_many :industries, :through => :merchant_categories_industries
+
   data_miner do
     tap "Brighter Planet's merchant category data", Earth.taps_server
   end
--- lib/earth/industry/sector.rb
+++ lib/earth/industry/sector.rb
@@ -1,9 +1,6 @@
 class Sector < ActiveRecord::Base
   set_primary_key :io_code
   
-  has_many :industies,     :through => :industries_sectors
-  has_many :product_lines, :through => :product_lines_sectors
-  
   data_miner do
     tap "Brighter Planet's input-output sector data", Earth.taps_server
   end
