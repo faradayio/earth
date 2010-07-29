a0a9efb27bbe0fe3be0046acb906b9ce60bbf5e7
Derek Kastner <dkastner@gmail.com>
2010-07-29 17:27:25 -0400

    Need a custom class name for MerchantCategoriesIndustries relation

--- lib/earth/industry/merchant_category.rb
+++ lib/earth/industry/merchant_category.rb
@@ -1,7 +1,7 @@
 class MerchantCategory < ActiveRecord::Base
   set_primary_key :mcc
 
-  has_many :merchant_categories_industries, :foreign_key => 'mcc'
+  has_many :merchant_categories_industries, :foreign_key => 'mcc', :class_name => 'MerchantCategoriesIndustries'
   has_many :industries, :through => :merchant_categories_industries
 
   data_miner do
