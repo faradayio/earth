642c31de5f3f880dee79149479658725f56661f0
Derek Kastner <dkastner@gmail.com>
2010-08-02 11:26:26 -0400

    Added relationships from MerchantCategoriesIndustries to IndustriesProductLines

--- lib/earth/industry/merchant_categories_industries.rb
+++ lib/earth/industry/merchant_categories_industries.rb
@@ -3,6 +3,8 @@ class MerchantCategoriesIndustries < ActiveRecord::Base
   
   belongs_to :merchant_category, :foreign_key => 'mcc'
   belongs_to :industry,          :foreign_key => 'naics_code'
+  has_many :industries_product_lines, :through => :industry, :class_name => 'IndustriesProductLines'
+  has_many :industries_sectors, :through => :industry, :class_name => 'IndustriesSectors'
   
   data_miner do
     tap "Brighter Planet's merchant category to industry dictionary", Earth.taps_server
