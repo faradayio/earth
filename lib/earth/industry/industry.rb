c39a0e1751baaceeafceb79fd7d4c001e4df1761
Derek Kastner <dkastner@gmail.com>
2010-08-06 17:49:03 -0500

    fortified Industry and Sector with extra relationships

--- lib/earth/industry/industry.rb
+++ lib/earth/industry/industry.rb
@@ -5,6 +5,8 @@ class Industry < ActiveRecord::Base
   has_many :product_lines, :through => :industries_product_lines
   has_many :industries_sectors, :foreign_key => 'naics_code'
   has_many :sectors, :through => :industries_sectors
+  has_many :merchant_categories_industries, :foreign_key => 'naics_code', 
+    :class_name => 'MerchantCategoriesIndustries'
   
   data_miner do
     tap "Brighter Planet's industry data", Earth.taps_server
--- lib/earth/industry/sector.rb
+++ lib/earth/industry/sector.rb
@@ -1,6 +1,8 @@
 class Sector < ActiveRecord::Base
   set_primary_key :io_code
 
+  has_many :product_lines_sectors, :class_name => 'ProductLinesSectors', :foreign_key => 'io_code'
+
   data_miner do
     schema Earth.database_options do
       string 'io_code'
