3268f6c972df79477348b11972895fa944dfb0e7
Derek Kastner <dkastner@gmail.com>
2010-08-15 20:56:50 -0400

    added name attribute to MerchantCategory, Gemfile.lock

--- lib/earth/industry/merchant_category.rb
+++ lib/earth/industry/merchant_category.rb
@@ -1,6 +1,8 @@
 class MerchantCategory < ActiveRecord::Base
   set_primary_key :mcc
 
+  alias :description, :name
+
   has_many :merchant_categories_industries, :foreign_key => 'mcc', :class_name => 'MerchantCategoriesIndustries'
   has_many :industries, :through => :merchant_categories_industries
 
