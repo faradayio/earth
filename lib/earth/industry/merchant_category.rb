0c65c4f56b61f4f8ebc66bb469eae83348e6ae6a
Derek Kastner <dkastner@gmail.com>
2010-08-15 21:50:25 -0400

    I really should have run tests

--- lib/earth/industry/merchant_category.rb
+++ lib/earth/industry/merchant_category.rb
@@ -1,10 +1,12 @@
 class MerchantCategory < ActiveRecord::Base
   set_primary_key :mcc
 
-  alias :description, :name
-
   has_many :merchant_categories_industries, :foreign_key => 'mcc', :class_name => 'MerchantCategoriesIndustries'
   has_many :industries, :through => :merchant_categories_industries
+
+  def name
+    description
+  end
 
   data_miner do
     schema Earth.database_options do
