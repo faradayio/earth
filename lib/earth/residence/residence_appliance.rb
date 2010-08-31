887d71f595fcb6bf25914683b72fd4ca21fedd0f
Derek Kastner <dkastner@gmail.com>
2010-08-31 11:33:31 -0400

    Added back methods for calculating annual energy use for Residence models

--- lib/earth/residence/residence_appliance.rb
+++ lib/earth/residence/residence_appliance.rb
@@ -1,6 +1,15 @@
 class ResidenceAppliance < ActiveRecord::Base
   set_primary_key :name
 
+  class << self
+    def annual_energy_from_electricity_for(appliance_plural)
+      appliance_name = appliance_plural.to_s.singularize
+      if appliance = find_by_name(appliance_name)
+        appliance.annual_energy_from_electricity
+      end
+    end
+  end
+
   data_miner do
     tap "Brighter Planet's residence appliance energy information", Earth.taps_server
   end
