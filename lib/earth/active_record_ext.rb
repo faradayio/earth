e84d6a55f089e23fc8f211ee0eee3416235a15f2
Seamus Abshere <seamus@abshere.net>
2010-09-07 23:06:50 -0500

    provide ActiveRecord::Base.run_data_miner_on_belongs_to_associations

---
+++ lib/earth/active_record_ext.rb
@@ -0,0 +1,8 @@
+::ActiveRecord::Base.class_eval do
+  def self.run_data_miner_on_belongs_to_associations
+    reflect_on_all_associations(:belongs_to).each do |a|
+      next if a.options[:polymorphic]
+      a.klass.run_data_miner!
+    end
+  end
+end
