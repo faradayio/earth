1f2888c0c03e7a18d077b91b93e5a801a98ab290
Derek Kastner <dkastner@gmail.com>
2010-09-08 18:21:52 -0400

    Sector no longer holds the key_map

--- lib/earth/industry/sector.rb
+++ lib/earth/industry/sector.rb
@@ -4,19 +4,6 @@ class Sector < ActiveRecord::Base
 
   has_many :product_lines_sectors, :class_name => 'ProductLinesSectors', :foreign_key => 'io_code'
 
-  class << self
-    def key_map
-      @key_map ||= sector_map.values.sort
-    end
-
-    def sector_map
-      @sector_map ||= Sector.all.inject({}) do |map, sector|
-        map[sector.description] = sector.io_code
-        map
-      end
-    end
-  end
-
   def self.schema_definition
     lambda do
       string 'io_code'
