f1343e8461bd30de6cd03e3b2470c0ebcb8537f1
Ian Hough <ian@brighterplanet.com>
2010-09-10 16:04:29 -0700

    This worked in data1, so I hope it works here too

--- lib/earth/hospitality/lodging_class/data_miner.rb
+++ lib/earth/hospitality/lodging_class/data_miner.rb
@@ -5,6 +5,15 @@ LodgingClass.class_eval do
       float  'emission_factor'
       string 'emission_factor_units'
     end
+    
+    process "Define some unit conversions" do
+      Conversions.register :pounds_co2e_per_room_night, :kilograms_co2e_per_room_night, 0.45359237
+    end
+
+    import "a list of rail classes and pre-calculated trip and fuel use characteristics",
+           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdGZZWmZtWEJlYzhRNXlPdWpBTldlcUE&hl=en&output=csv' do
+      key   'name'
+      store 'emission_factor', :from_units => :pounds_co2e_per_room_night, :to_units => :kilograms_co2e_per_room_night
+    end
   end
 end
-
