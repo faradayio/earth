01d64ef92a9a940250fd9649f24262eaab2db256
Ian Hough <ian@brighterplanet.com>
2010-09-21 15:44:48 -0700

    I think these need a primary_key

--- lib/earth/industry/merchant_category_industry.rb
+++ lib/earth/industry/merchant_category_industry.rb
@@ -1,5 +1,6 @@
 class MerchantCategoryIndustry < ActiveRecord::Base
   extend Earth::Base
+  set_primary_key :row_hash
   
   belongs_to :merchant_category, :foreign_key => 'mcc'
   belongs_to :industry,          :foreign_key => 'naics_code'
@@ -8,6 +9,7 @@ class MerchantCategoryIndustry < ActiveRecord::Base
 
   def self.schema_definition
     lambda do
+      string 'row_hash'
       string 'mcc'
       float  'ratio'
       string 'naics_code'
