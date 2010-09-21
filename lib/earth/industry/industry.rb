cae69efb279e3ca96d3fe3eb18390497a41ad3a0
Ian Hough <ian@brighterplanet.com>
2010-09-21 15:43:14 -0700

    Re-order and add whitespace to clarify associations

--- lib/earth/industry/industry.rb
+++ lib/earth/industry/industry.rb
@@ -2,11 +2,13 @@ class Industry < ActiveRecord::Base
   extend Earth::Base
   set_primary_key :naics_code
   
+  has_many :merchant_category_industries, :foreign_key => 'naics_code'
+  
   has_many :industry_product_lines, :foreign_key => 'naics_code'
-  has_many :product_lines, :through => :industries_product_lines
+  has_many :product_lines, :through => :industry_product_lines
+  
   has_many :industry_sectors, :foreign_key => 'naics_code'
   has_many :sectors, :through => :industry_sectors
-  has_many :merchant_category_industries, :foreign_key => 'naics_code'
   
   def self.schema_definition
     lambda do
