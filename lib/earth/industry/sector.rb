caa2cf65dfca6a179d7b8e979a50b127aabf9bd9
Ian Hough <ian@brighterplanet.com>
2010-09-24 12:27:59 -0700

    Explicitly state association between Sector and IndustrySectors, even though things seem to work without it

--- lib/earth/industry/sector.rb
+++ lib/earth/industry/sector.rb
@@ -2,6 +2,8 @@ class Sector < ActiveRecord::Base
   extend Earth::Base
   set_primary_key :io_code
 
+  has_many :industry_sectors, :foreign_key => 'io_code'
+  
   def self.schema_definition
     lambda do
       string 'io_code'
