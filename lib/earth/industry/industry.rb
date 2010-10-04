f723ba8a6decefc90a3608bd410cc697652c5ec8
Derek Kastner <dkastner@gmail.com>
2010-10-04 16:06:41 -0400

    Added Industry#trade_industry?

--- lib/earth/industry/industry.rb
+++ lib/earth/industry/industry.rb
@@ -20,4 +20,9 @@ class Industry < ActiveRecord::Base
   data_miner do
     Industry.define_schema(self)
   end
+
+  def trade_industry?
+    prefix = naics_code.to_s[0,2]
+    %w{42 44 45}.include?(prefix)
+  end
 end
