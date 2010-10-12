36f5d28c34bb03f787d540dddf5b32a2c529ba30
Ian Hough <ian@brighterplanet.com>
2010-10-12 13:46:23 -0700

    Add flight route inefficiency factors to Country

--- lib/earth/locality/country.rb
+++ lib/earth/locality/country.rb
@@ -1,6 +1,8 @@
 class Country < ActiveRecord::Base
   set_primary_key :iso_3166_code
   
+  falls_back_on :flight_route_inefficiency_factor => 1.10 # for international flights - this is the larger (European) factor from Kettunen et al. (2005) http://www.atmseminar.org/seminarContent/seminar6/papers/p_055_MPM.pdf
+  
   data_miner do
     tap "Brighter Planet's sanitized countries list", Earth.taps_server
   end
--- lib/earth/locality/country/data_miner.rb
+++ lib/earth/locality/country/data_miner.rb
@@ -1,8 +1,9 @@
 Country.class_eval do
   data_miner do
     schema Earth.database_options do
-      string   'iso_3166_code'
-      string   'name'
+      string 'iso_3166_code'
+      string 'name'
+      float  'flight_route_inefficiency_factor'
     end
     
     import 'the official ISO country list',
@@ -14,6 +15,11 @@ Country.class_eval do
       key   'iso_3166_code', :field_number => 1
       store 'name', :field_number => 0
     end
+    
+    import 'country-specific flight route inefficiency factors derived from Ketteunen et al. (2005)',
+           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdG0yc3BxYUkybWV5M3RKb2t4X0JUOFE&hl=en&single=true&gid=0&output=csv' do
+      key   'iso_3166_code'
+      store 'flight_route_inefficiency_factor'
+    end
   end
 end
-
