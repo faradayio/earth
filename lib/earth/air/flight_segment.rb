0f94a3a8cd3754e4f3052ccca61698e8e8a310e2
Ian Hough <ian@brighterplanet.com>
2010-10-13 13:43:52 -0700

    Add a distance fallback to flight_segment

--- lib/earth/air/flight_segment.rb
+++ lib/earth/air/flight_segment.rb
@@ -15,7 +15,8 @@ class FlightSegment < ActiveRecord::Base
   belongs_to :service_class,                                                       :class_name => 'FlightService'
   belongs_to :domesticity,                                                         :class_name => 'FlightDomesticity'
   
-  falls_back_on :seats             => lambda { weighted_average(:seats,         :weighted_by => :passengers) }, # 144.15653537046   data1 10-12-2010
+  falls_back_on :distance          => lambda { weighted_average(:distance,      :weighted_by => :passengers) }, # 2077.1205         data1 10-12-2010
+                :seats             => lambda { weighted_average(:seats,         :weighted_by => :passengers) }, # 144.15653537046   data1 10-12-2010
                 :load_factor       => lambda { weighted_average(:load_factor,   :weighted_by => :passengers) }, # 0.78073233770097  data1 10-12-2010
                 :freight_share     => lambda { weighted_average(:freight_share, :weighted_by => :passengers) }  # 0.022567224170157 data1 10-12-2010
 
