2631b75500d40a545bb8463e77afaf496cb53872
Ian Hough <ian@brighterplanet.com>
2010-10-18 17:30:21 -0700

    Standardize distance classes

--- lib/earth/air/flight_distance_class/data_miner.rb
+++ lib/earth/air/flight_distance_class/data_miner.rb
@@ -6,11 +6,10 @@ FlightDistanceClass.class_eval do
       string 'distance_units'
     end
     
-    import "a list of Brighter Planet-defined distance classes",
+    import "a list of distance classes taken from the WRI business travel tool",
            :url => 'http://static.brighterplanet.com/science/data/transport/air/distance_classes/distance_classes.csv' do
       key   'name'
-      store 'distance', :units_field_name => 'units', :to_units => :kilometres
+      store 'distance', :units_field_name => 'distance_units'
     end
   end
 end
-
