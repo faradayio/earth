0edfaf157f138ad90cf88e42f927757dcc9bc519
Ian Hough <ian@brighterplanet.com>
2010-10-26 16:50:47 -0700

    Temporarily 'fixing' FlightSegment - Aircraft association...

--- lib/earth/air/aircraft.rb
+++ lib/earth/air/aircraft.rb
@@ -3,7 +3,7 @@ class Aircraft < ActiveRecord::Base
   
   belongs_to :aircraft_class, :foreign_key => 'class_code',             :primary_key => 'code'
   belongs_to :manufacturer,   :foreign_key => 'icao_manufacturer_name', :primary_key => 'name',     :class_name => 'AircraftManufacturer'
-  has_many   :segments,       :foreign_key => 'bts_code',               :primary_key => 'row_hash', :class_name => 'FlightSegment'
+  has_many   :segments,       :foreign_key => 'aircraft_bts_code',               :primary_key => 'row_hash', :class_name => 'FlightSegment'
   
   falls_back_on :m3            => lambda { weighted_average(:m3,            :weighted_by => [:segments, :passengers]) }, # 9.73423082858437e-08   r7110: 8.6540464368905e-8      r6972: 8.37e-8
                 :m2            => lambda { weighted_average(:m2,            :weighted_by => [:segments, :passengers]) }, # -0.000134350543484608  r7110: -0.00015337661447817    r6972: -4.09e-5
