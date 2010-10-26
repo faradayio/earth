648f08312d0d7ba94995f85961f1d4c55786ac7f
Ian Hough <ian@brighterplanet.com>
2010-10-25 17:30:42 -0700

    Fixing :foreign_key

--- lib/earth/air/aircraft_manufacturer.rb
+++ lib/earth/air/aircraft_manufacturer.rb
@@ -1,7 +1,7 @@
 class AircraftManufacturer < ActiveRecord::Base
   set_primary_key :name
   
-  has_many :aircraft, :foreign_key => 'icao_manufacturer_name'
+  has_many :aircraft, :foreign_key => 'name'
   
   data_miner do
     tap "Brighter Planet's aircraft manufacturer data", Earth.taps_server
