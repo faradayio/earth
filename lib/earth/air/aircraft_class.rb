90637b7d6326209b551390adfeab91e96f3ef3a2
Ian Hough <ian@brighterplanet.com>
2010-10-29 16:01:45 -0700

    Cut unused associations

--- lib/earth/air/aircraft_class.rb
+++ lib/earth/air/aircraft_class.rb
@@ -1,7 +1,7 @@
 class AircraftClass < ActiveRecord::Base
   set_primary_key :code
   
-  has_many :aircraft, :foreign_key => 'code', :primary_key => 'bp_code'
+  # has_many :aircraft, :foreign_key => 'class_code', :primary_key => 'code'
   
   data_miner do
     tap "Brighter Planet's aircraft class data", Earth.taps_server
--- lib/earth/air/aircraft_manufacturer.rb
+++ lib/earth/air/aircraft_manufacturer.rb
@@ -1,7 +1,7 @@
 class AircraftManufacturer < ActiveRecord::Base
   set_primary_key :name
   
-  has_many :aircraft, :foreign_key => 'name', :primary_key => 'bp_code'
+  # has_many :aircraft, :foreign_key => 'name', :primary_key => 'bp_code'
   
   data_miner do
     tap "Brighter Planet's aircraft manufacturer data", Earth.taps_server
