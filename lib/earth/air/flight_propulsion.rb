ca12ee0606915bcdd614810b81341734ca2fc130
Derek Kastner <dkastner@gmail.com>
2010-11-09 15:09:05 -0500

    Removed FlightPropulsion

--- lib/earth/air/flight_propulsion.rb
+++
@@ -1,7 +0,0 @@
-class FlightPropulsion < ActiveRecord::Base
-  set_primary_key :name
-  
-  data_miner do
-    tap "Brighter Planet's sanitized flight propulsion data", Earth.taps_server
-  end
-end
