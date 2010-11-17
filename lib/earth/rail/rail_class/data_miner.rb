075b7dd2873ff3efe1073838d8df9eb421bf087c
Ian Hough <ian@brighterplanet.com>
2010-11-17 13:04:17 -0800

    Fallbacks are now in the source data as "US average"; cut unused duration attribute

--- lib/earth/rail/rail_class/data_miner.rb
+++ lib/earth/rail/rail_class/data_miner.rb
@@ -2,18 +2,16 @@ RailClass.class_eval do
   data_miner do
     schema Earth.database_options do
       string 'name'
+      string 'description'
+      float  'passengers'
       float  'distance'
       string 'distance_units'
-      float  'passengers'
       float  'speed'
       string 'speed_units'
-      float  'duration'
-      string 'duration_units'
       float  'electricity_intensity'
       string 'electricity_intensity_units'
       float  'diesel_intensity'
       string 'diesel_intensity_units'
-      string 'description'
     end
     
     process "Define some unit conversions" do
@@ -24,13 +22,11 @@ RailClass.class_eval do
            :url => 'http://static.brighterplanet.com/science/data/transport/rail/rail_classes.csv' do
       key   'name'
       store 'description'
-      store 'distance', :from_units => :miles, :to_units => :kilometres
-      store 'speed', :from_units => :miles, :to_units => :kilometres
-      store 'duration'
       store 'passengers'
-      store 'electricity_intensity', :field_name => 'electricity_per_vehicle_mile', :from_units => :miles, :to_units => :kilometres
-      store 'diesel_intensity', :field_name => 'diesel_per_vehicle_mile', :from_units => :gallons_per_mile, :to_units => :litres_per_kilometre
+      store 'distance', :units_field_name => 'distance_units'
+      store 'speed', :units_field_name => 'speed_units'
+      store 'electricity_intensity', :units_field_name => 'electricity_intensity_units'
+      store 'diesel_intensity', :units_field_name => 'diesel_intensity_units'
     end
   end
 end
-
