d0bffd1f5b119821b28b4c1a5e766108fa26e7bb
Ian Hough <ian@brighterplanet.com>
2010-12-10 16:39:15 -0500

    Fallback values - I tried to calculate these but it gave errors like "can't find table carriers"

--- lib/earth/shipping/carrier.rb
+++ lib/earth/shipping/carrier.rb
@@ -1,6 +1,8 @@
 class Carrier < ActiveRecord::Base
   set_primary_key :name
   
+  falls_back_on :corporate_emission_factor => 0.318#Carrier.average(:corporate_emission_factor)
+  
   data_miner do
     tap "Brighter Planet's shipping company data", Earth.taps_server
   end
--- lib/earth/shipping/shipment_mode.rb
+++ lib/earth/shipping/shipment_mode.rb
@@ -1,6 +1,9 @@
 class ShipmentMode < ActiveRecord::Base
   set_primary_key :name
   
+  falls_back_on :route_inefficiency_factor => 1.05,#ShipmentMode.where(not(:name => 'courier')).average(:route_inefficiency_factor),
+                :transport_emission_factor => 3.0#ShipmentMode.where(not(:name => 'courier')).average(:route_inefficiency_factor)
+  
   data_miner do
     tap "Brighter Planet's shipment mode data", Earth.taps_server
   end
