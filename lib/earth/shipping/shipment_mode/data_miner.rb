d71c655c0b4f64c0516658536209eecea64807f8
Ian Hough <ian@brighterplanet.com>
2010-12-10 15:26:37 -0500

    Import blocks for shipping data

--- lib/earth/shipping/shipment_mode/data_miner.rb
+++ lib/earth/shipping/shipment_mode/data_miner.rb
@@ -6,5 +6,12 @@ ShipmentMode.class_eval do
       float  'transport_emission_factor'
       string 'transport_emission_factor_units'
     end
+    
+    import "a list of shipment modes and their characteristics",
+           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdGR2a2RYcEg2RnRKbkRQcXc0ZkQ5U0E&hl=en&single=true&gid=0&output=csv' do
+      key   'name'
+      store 'route_inefficiency_factor'
+      store 'transport_emission_factor', :units_field_name => 'transport_emission_factor_units'
+    end
   end
 end
--- lib/earth/shipping/shipping_company/data_miner.rb
+++ lib/earth/shipping/shipping_company/data_miner.rb
@@ -5,5 +5,11 @@ ShippingCompany.class_eval do
       float  'corporate_emission_factor'
       string 'corporate_emission_factor_units'
     end
+    
+    import "a list of shipping companies and their characteristics",
+           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdG1ONU1HZDdZTFJNclFYVkRzR0k5Z2c&hl=en&single=true&gid=0&output=csv' do
+      key   'name'
+      store 'corporate_emission_factor', :units_field_name => 'corporate_emission_factor_units'
+    end
   end
 end
