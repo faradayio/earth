eb041bdaad5f391010cfa52d6918c4d9c8fe84be
Ian Hough <ian@brighterplanet.com>
2010-12-12 18:41:38 -0500

    Real fallback transport emission factor

--- lib/earth/shipping/shipment_mode.rb
+++ lib/earth/shipping/shipment_mode.rb
@@ -2,7 +2,7 @@ class ShipmentMode < ActiveRecord::Base
   set_primary_key :name
   
   falls_back_on :route_inefficiency_factor => 1.05,#ShipmentMode.where(not(:name => 'courier')).average(:route_inefficiency_factor),
-                :transport_emission_factor => 3.0#ShipmentMode.where(not(:name => 'courier')).average(:route_inefficiency_factor)
+                :transport_emission_factor => 0.0007696#ShipmentMode.where(not(:name => 'courier')).average(:route_inefficiency_factor)
   
   data_miner do
     tap "Brighter Planet's shipment mode data", Earth.taps_server
