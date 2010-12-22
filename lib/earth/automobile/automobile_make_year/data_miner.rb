72933a39d6ad3e304c3cb0218af1104c88b4b667
Ian Hough <ian@brighterplanet.com>
2010-12-22 13:53:02 -0500

    Verification for AutomobileMakeYear

--- lib/earth/automobile/automobile_make_year/data_miner.rb
+++ lib/earth/automobile/automobile_make_year/data_miner.rb
@@ -40,6 +40,37 @@ AutomobileMakeYear.class_eval do
         UPDATE automobile_make_years SET automobile_make_years.volume = (SELECT SUM(automobile_make_fleet_years.volume) FROM automobile_make_fleet_years WHERE automobile_make_fleet_years.make_year_name = automobile_make_years.name)
       }
     end
+    
+    verify "Year should be between 1978 and 2007" do
+      AutomobileMakeYear.all.each do |make_year|
+        unless make_year.year > 1977 and make_year.year < 2008
+          raise "Invalid year for AutomobileMakeYear #{make_year.name}: #{make_year.year} (should be between 1978 and 2007)"
+        end
+      end
+    end
+    
+    verify "Fuel efficiency should be greater than zero" do
+      AutomobileMakeYear.all.each do |make_year|
+        unless make_year.fuel_efficiency > 0
+          raise "Invalid fuel efficiency for AutomobileMakeYear #{make_year.name}: #{make_year.fuel_efficiency} (should be > 0)"
+        end
+      end
+    end
+    
+    verify "Fuel efficiency units should be kilometres per litre" do
+      AutomobileMakeYear.all.each do |make_year|
+        unless make_year.fuel_efficiency_units == "kilometres_per_litre"
+          raise "Invalid fuel efficiency units for AutomobileMakeYear #{make_year.name}: #{make_year.fuel_efficiency_units} (should be kilometres_per_litre)"
+        end
+      end
+    end
+    
+    verify "Volume should be greater than zero" do
+      AutomobileMakeYear.all.each do |make_year|
+        unless make_year.volume > 0
+          raise "Invalid volume for AutomobileMakeYear #{make_year.name}: #{make_year.volume} (should be > 0)"
+        end
+      end
+    end
   end
 end
-
