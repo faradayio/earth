025e1151b52e6b5f27322dbc2af8a9ea3615e324
Ian Hough <ian@brighterplanet.com>
2010-12-22 18:11:34 -0500

    Fix derivation of AutomobileMakeModelYear city/highway fuel efficiency

--- lib/earth/automobile/automobile_make_model_year/data_miner.rb
+++ lib/earth/automobile/automobile_make_model_year/data_miner.rb
@@ -32,12 +32,13 @@ AutomobileMakeModelYear.class_eval do
     # for example, 20km/l for a toyota prius 2006 vs. 13km/l if you use make years
     process "Calculate city and highway fuel efficiency from automobile make model year variants" do
       AutomobileMakeModelYearVariant.run_data_miner!
-      automobile_make_model_years = AutomobileMakeModelYear.arel_table
-      automobile_make_model_year_variants = AutomobileMakeModelYearVariant.arel_table
-      conditional_relation = automobile_make_model_years[:name].eq(automobile_make_model_year_variants[:make_model_year_name])
+      model_years = AutomobileMakeModelYear.arel_table
+      variants = AutomobileMakeModelYearVariant.arel_table
+      conditional_relation = model_years[:name].eq(variants[:make_model_year_name])
       %w{ city highway }.each do |i|
+        null_check = variants[:"fuel_efficiency_#{i}"].not_eq(nil)
         # sabshere 12/6/10 careful, don't use AutomobileMakeModelYearVariant.where here or you will be forced into projecting *
-        relation = automobile_make_model_year_variants.where(conditional_relation).where("`automobile_make_model_year_variants`.`fuel_efficiency_#{i}` IS NOT NULL").project("AVG(`automobile_make_model_year_variants`.`fuel_efficiency_#{i}`)")
+        relation = variants.project(variants[:"fuel_efficiency_#{i}"].average).where(conditional_relation).where(null_check)
         update_all "fuel_efficiency_#{i} = (#{relation.to_sql})"
         update_all "fuel_efficiency_#{i}_units = 'kilometres_per_litre'"
       end
