042a604396c305f8c46c36b20c170b58bdc8e094
Ian Hough <ian@brighterplanet.com>
2010-12-22 12:32:28 -0500

    Verification for AutomobileMakeModel

--- lib/earth/automobile/automobile_make_model/data_miner.rb
+++ lib/earth/automobile/automobile_make_model/data_miner.rb
@@ -34,6 +34,27 @@ AutomobileMakeModel.class_eval do
         update_all "fuel_efficiency_#{i}_units = 'kilometres_per_litre'"
       end
     end
+    
+    verify "Fuel efficiencies should be greater than zero" do
+      AutomobileMakeModel.all.each do |model|
+        %w{ city highway }.each do |type|
+          fuel_efficiency = model.send(:"fuel_efficiency_#{type}")
+          unless fuel_efficiency > 0
+            raise "Invalid fuel efficiency #{type} for AutomobileMakeModel #{model.name}: #{fuel_efficiency} (should be > 0)"
+          end
+        end
+      end
+    end
+    
+    verify "Fuel efficiency units should be kilometres per litre" do
+      AutomobileMakeModel.all.each do |model|
+        %w{ city highway }.each do |type|
+          units = model.send(:"fuel_efficiency_#{type}_units")
+          unless units == "kilometres_per_litre"
+            raise "Invalid fuel efficiency #{type} units for AutomobileMakeModel #{model.name}: #{units} (should be kilometres_per_litre)"
+          end
+        end
+      end
+    end
   end
 end
-
