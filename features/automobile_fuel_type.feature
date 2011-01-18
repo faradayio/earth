bef98dad66d8eac28a71e4d016c54d4585a5ffa6
Ian Hough <ian@brighterplanet.com>
2011-01-18 12:37:08 -0800

    Calculate AutomobileFuelType annual_distance and emission_factor automatically

--- features/automobile_fuel_type.feature
+++ features/automobile_fuel_type.feature
@@ -3,19 +3,14 @@ Feature: Data import for AutomobileFuelType
   I want to import AutomobileFuelType data
   So that I can perform fuel type-based calculations
 
-  Scenario: Successfully verifying that annual distance > 0
+  Scenario: Successfully verifying that annual distance and emission factor > 0
     Given a "AutomobileFuelType" data import fetches results listed in "automobile_fuel_type_good"
-    When a data import verifies "Annual distance should be greater than zero"
+    When a data import verifies "Annual distance and emission factor should be greater than zero"
     Then the verification should be successful
 
-  Scenario: Successfully verifying that annual distance units are never missing
+  Scenario: Successfully verifying that annual distance units are kilometres
     Given a "AutomobileFuelType" data import fetches results listed in "automobile_fuel_type_good"
-    When a data import verifies "Annual distance units should never be missing"
-    Then the verification should be successful
-
-  Scenario: Successfully verifying that emission factor >= 0
-    Given a "AutomobileFuelType" data import fetches results listed in "automobile_fuel_type_good"
-    When a data import verifies "Emission factor should be zero or more"
+    When a data import verifies "Annual distance units should be kilometres"
     Then the verification should be successful
 
   Scenario: Successfully verifying that emission factor units are never missing
@@ -23,19 +18,14 @@ Feature: Data import for AutomobileFuelType
     When a data import verifies "Emission factor units should never be missing"
     Then the verification should be successful
 
-  Scenario: Failing to verify that annual distance > 0
+  Scenario: Failing to verify that annual distance and emission factor > 0
     Given a "AutomobileFuelType" data import fetches results listed in "automobile_fuel_type_bad"
-    When a data import verifies "Annual distance should be greater than zero"
+    When a data import verifies "Annual distance and emission factor should be greater than zero"
     Then the verification should not be successful
 
-  Scenario: Failing to verify that annual distance units are never missing
+  Scenario: Failing to verify that annual distance units are kilometres
     Given a "AutomobileFuelType" data import fetches results listed in "automobile_fuel_type_bad"
-    When a data import verifies "Annual distance units should never be missing"
-    Then the verification should not be successful
-
-  Scenario: Failing to verify that emission factor >= 0
-    Given a "AutomobileFuelType" data import fetches results listed in "automobile_fuel_type_bad"
-    When a data import verifies "Emission factor should be zero or more"
+    When a data import verifies "Annual distance units should be kilometres"
     Then the verification should not be successful
 
   Scenario: Failing to verify that emission factor units are never missing
--- lib/earth/automobile/automobile_fuel_type/data_miner.rb
+++ lib/earth/automobile/automobile_fuel_type/data_miner.rb
@@ -3,50 +3,97 @@ AutomobileFuelType.class_eval do
     schema Earth.database_options do
       string   'code'
       string   'name'
-      string   'fuel_common_name'
+      string   'fuel_name_for_distance'
+      string   'fuel_name_for_efs'
+      float    'blend_portion'
       float    'emission_factor'
       string   'emission_factor_units'
       float    'annual_distance'
       string   'annual_distance_units'
     end
     
-    import "a pre-calculated emission factor and average annual distance for each fuel",
+    import "a list of fuel type codes and attributes",
            :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdDlqeU9vQkVkNG1NZXV4WklKTjJkU3c&hl=en&single=true&gid=0&output=csv' do
       key   'code'
       store 'name'
-      store 'fuel_common_name'
-      store 'emission_factor', :units_field_name => 'emission_factor_units'
+      store 'fuel_name_for_distance'
+      store 'fuel_name_for_efs'
+      store 'blend_portion'
     end
     
     process "Calculate annual distance from AutomobileTypeFuelAge" do
       AutomobileTypeFuelAge.run_data_miner!
       ages = AutomobileTypeFuelAge.arel_table
       types = AutomobileFuelType.arel_table
-      conditional_relation = ages[:fuel_common_name].eq(types[:fuel_common_name])
+      conditional_relation = ages[:fuel_common_name].eq(types[:fuel_name_for_distance])
       update_all "annual_distance = (#{AutomobileTypeFuelAge.weighted_average_relation(:annual_distance, :weighted_by => :vehicles).where(conditional_relation).to_sql})"
       update_all "annual_distance_units = 'kilometres'"
     end
     
-    verify "Annual distance should be greater than zero" do
+    process "Calculate emission factor from FuelYear, AutomobileTypeFuelYear, and GreenhouseGas" do
+      FuelYear.run_data_miner!
+      AutomobileTypeFuelYear.run_data_miner!
+      GreenhouseGas.run_data_miner!
+      
+      co2_gwp = GreenhouseGas.find_by_abbreviation("co2").global_warming_potential
+      ch4_gwp = GreenhouseGas.find_by_abbreviation("ch4").global_warming_potential
+      n2o_gwp = GreenhouseGas.find_by_abbreviation("n2o").global_warming_potential
+      
+      latest_year = FuelYear.all.collect{|x| x.year }.max
+      
+      fuel_years = FuelYear.arel_table
+      type_fuel_years = AutomobileTypeFuelYear.arel_table
+      fuel_types = AutomobileFuelType.arel_table
+      
       AutomobileFuelType.all.each do |fuel_type|
-        unless fuel_type.annual_distance > 0.0
-          raise "Invalid annual_distance for AutomobileFuelType #{fuel_type.name}: #{fuel_type.annual_distance} (should be > 0)"
+        current_name = fuel_type.fuel_name_for_efs
+        
+        unless current_name.nil?
+          co2_factor_sql = fuel_years.project(fuel_years[:co2_emission_factor]).where(fuel_years[:fuel_common_name].eq(fuel_types[:fuel_name_for_efs]).and(fuel_years[:year].eq(latest_year))).to_sql
+          ch4_factor_sql = AutomobileTypeFuelYear.weighted_average_relation(:ch4_emission_factor, :weighted_by => :total_travel).where(type_fuel_years[:fuel_common_name].eq(fuel_types[:fuel_name_for_efs]).and(type_fuel_years[:year].eq(latest_year))).to_sql
+          n2o_factor_sql = AutomobileTypeFuelYear.weighted_average_relation(:n2o_emission_factor, :weighted_by => :total_travel).where(type_fuel_years[:fuel_common_name].eq(fuel_types[:fuel_name_for_efs]).and(type_fuel_years[:year].eq(latest_year))).to_sql
+          hfc_factor_sql = AutomobileTypeFuelYear.weighted_average_relation(:hfc_emission_factor, :weighted_by => :total_travel).where(type_fuel_years[:fuel_common_name].eq(fuel_types[:fuel_name_for_efs]).and(type_fuel_years[:year].eq(latest_year))).to_sql
+          
+          connection.execute %{
+            UPDATE automobile_fuel_types
+            SET emission_factor = (((#{co2_factor_sql}) * #{co2_gwp} * (1 - blend_portion)) + ((#{ch4_factor_sql}) * #{ch4_gwp}) + ((#{n2o_factor_sql}) * #{n2o_gwp}) + (#{hfc_factor_sql}))
+            WHERE automobile_fuel_types.fuel_name_for_efs = '#{current_name}'
+          }
+          connection.execute %{
+            UPDATE automobile_fuel_types
+            SET emission_factor_units = 'kilograms_co2e_per_litre'
+            WHERE automobile_fuel_types.fuel_name_for_efs = '#{current_name}'
+          }
         end
       end
     end
     
-    verify "Annual distance units should never be missing" do
+    process "Calculate emission factor for electricity from EgridSubregion and EgridRegion" do
+      EgridSubregion.run_data_miner!
+      EgridRegion.run_data_miner!
+      electricity_ef = (EgridSubregion.find_by_abbreviation("US").electricity_emission_factor / (1 - EgridRegion.find_by_name("US").loss_factor))
+      connection.execute %{
+        UPDATE automobile_fuel_types
+        SET emission_factor = #{electricity_ef}
+        WHERE automobile_fuel_types.name = 'electricity'
+      }
+    end
+    
+    verify "Annual distance and emission factor should be greater than zero" do
       AutomobileFuelType.all.each do |fuel_type|
-        if fuel_type.annual_distance_units.nil?
+        %w{ annual_distance emission_factor }.each do |attribute|
+          value = fuel_type.send(:"#{attribute}")
+          unless value > 0
+            raise "Invalid #{attribute} for AutomobileFuelType #{fuel_type.name}: #{value} (should be > 0)"
+          end
+        end
+      end
+    end
+    
+    verify "Annual distance units should be kilometres" do
+      AutomobileFuelType.all.each do |fuel_type|
+        unless fuel_type.annual_distance_units == "kilometres"
           raise "Missing annual distance units for AutomobileFuelType #{fuel_type.name}"
-        end
-      end
-    end
-    
-    verify "Emission factor should be zero or more" do
-      AutomobileFuelType.all.each do |fuel_type|
-        unless fuel_type.emission_factor >= 0.0
-          raise "Invalid emission_factor for AutomobileFuelType #{fuel_type.name}: #{fuel_type.emission_factor} (should be >= 0)"
         end
       end
     end
