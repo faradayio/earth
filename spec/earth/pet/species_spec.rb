f95d4c955677aad2398da9ed50ce05c42cacd330
Derek Kastner <dkastner@gmail.com>
2011-01-25 13:10:19 -0500

    Prevent divide-by-zero errors on fallbacks

---
+++ spec/earth/pet/species_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'Species' do
+  describe 'fallback' do
+    describe 'marginal_dietary_requirement' do
+      it 'should return 0 when there is no data' do
+        Earth.init :pet, :apply_schemas => true
+        Species.delete_all
+        Species.fallback.marginal_dietary_requirement.should == 0
+      end
+    end
+  end
+end
+
