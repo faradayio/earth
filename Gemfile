18719b0dfa5f7edf154ac94266edc097c4b1890f
Seamus Abshere <seamus@abshere.net>
2011-01-26 19:11:50 -0600

    get closer to bundler recommended gem layout

--- Gemfile
+++ Gemfile
@@ -1,10 +1,4 @@
-gem 'data_miner', :path => ENV['LOCAL_DATA_MINER'] if ENV['LOCAL_DATA_MINER']
-gem 'sniff', :path => ENV['LOCAL_SNIFF'] if ENV['LOCAL_SNIFF']
+source "http://rubygems.org"
 
-source :rubygems
-
-gemspec :path => '.'
-
-unless RUBY_VERSION >= '1.9'
-  gem 'fastercsv'
-end
+# Specify your gem's dependencies in earth.gemspec
+gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,11 @@ ActiveRecord::Base.establish_connection :adapter => 'sqlite3',
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'earth' # we do require Earth to live
 
+require 'logger'
+test_log = File.open('test.log', 'w')
+test_log.sync = true
+DataMiner.logger = Logger.new test_log
+
 RSpec.configure do |c|
   c.filter_run_excluding :slow => true
 end
