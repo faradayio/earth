4bd801454abbec284ec881ae5c9e945761caadee
Seamus Abshere <seamus@abshere.net>
2011-01-28 10:13:09 -0600

    help out with inflections

--- lib/earth/inflectors.rb
+++ lib/earth/inflectors.rb
@@ -1,9 +1,11 @@
 ActiveSupport::Inflector.inflections do |inflect|
-  inflect.uncountable %w(aircraft aircraft Aircraft airline_aircraft AirlineAircraft)
+  inflect.uncountable 'aircraft'
   inflect.uncountable 'commons'
   inflect.uncountable 'food'
   inflect.uncountable 'shelter'
   inflect.uncountable 'transportation'
-  inflect.uncountable 'press_coverage'
+  inflect.uncountable 'species'
   inflect.irregular 'foot', 'feet'
+  inflect.plural /(gas)\z/i, '\1es'
+  inflect.singular /(gas)es\z/i, '\1'
 end
