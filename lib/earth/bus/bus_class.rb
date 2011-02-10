92cbc9830736ce9697b86e87f388a94287dbd3a4
Ian Hough <ian@brighterplanet.com>
2011-02-09 16:23:59 -0800

    Use latest bus_class fallbacks

--- lib/earth/bus/bus_class.rb
+++ lib/earth/bus/bus_class.rb
@@ -1,14 +1,14 @@
 class BusClass < ActiveRecord::Base
   set_primary_key :name
   
-  # https://brighterplanet.sifterapp.com/projects/30/issues/454
-  falls_back_on :distance => 5.45.miles.to(:kilometres),
-                :passengers => 8.25,
-                :speed => 19.67.miles.to(:kilometres),
-                :diesel_intensity => 0.143.gallons_per_mile.to(:litres_per_kilometre),
-                :alternative_fuels_intensity => 0.0517.gallons_per_mile.to(:litres_per_kilometre),
-                :air_conditioning_emission_factor => 0.0203.pounds_per_mile.to(:kilograms_per_kilometre)
-
+  # https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdHY0R0hjZnhRaTIxTnpvRk1HNThwUmc&hl=en&output=html
+  falls_back_on :passengers => 7.485,
+                :distance => 4.9726.miles.to(:kilometres),
+                :speed => 19.849.miles.to(:kilometres),
+                :diesel_intensity => 0.13836.gallons_per_mile.to(:litres_per_kilometre),
+                :alternative_fuels_intensity => 0.04632038.gallons_per_mile.to(:litres_per_kilometre),
+                :air_conditioning_emission_factor => 0.04779 / 1.miles.to(:kilometres)
+  
   data_miner do
     tap "Brighter Planet's sanitized bus class data", Earth.taps_server
   end
