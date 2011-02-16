5a445d66186871c4f94bad04b2bc767883116a0b
Ian Hough <ian@brighterplanet.com>
2011-02-16 11:21:37 -0800

    Cut automobile_fuel_spec tests that take a long time to run

--- spec/earth/automobile/automobile_fuel_spec.rb
+++ spec/earth/automobile/automobile_fuel_spec.rb
@@ -6,15 +6,14 @@ describe AutomobileFuel do
   before :all do
     AutomobileFuel.run_data_miner!
   end
-
+  
   it 'imports data without problems' do
     AutomobileFuel.all.count.should > 0
   end
-  it 'correctly assigns hfc_emission_factor' do
-    AutomobileTypeFuelAge.run_data_miner!
-    AutomobileTypeFuelYear.run_data_miner!
-    AutomobileTypeYear.run_data_miner!
-    AutomobileFuel.first.hfc_emission_factor.should == 0.0
-  end
+  # it 'correctly assigns hfc_emission_factor' do
+  #   AutomobileTypeFuelAge.run_data_miner!
+  #   AutomobileTypeFuelYear.run_data_miner!
+  #   AutomobileTypeYear.run_data_miner!
+  #   AutomobileFuel.first.hfc_emission_factor.should == 0.0
+  # end
 end
-
