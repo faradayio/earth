6097b5547be16bb52b66f4d8922c73a949e7538f
Ian Hough <ian@brighterplanet.com>
2011-02-16 11:01:56 -0800

    Add hfc_emission_factor to AutomobileTypeYear

--- lib/earth/automobile/automobile_type_year.rb
+++ lib/earth/automobile/automobile_type_year.rb
@@ -1,26 +1,9 @@
 class AutomobileTypeYear < ActiveRecord::Base
   set_primary_key :name
   
-  has_many :automobile_type_fuel_years, :foreign_key => 'type_year_name'
+  has_many :type_fuel_years, :class_name => 'AutomobileTypeFuelYear', :foreign_key => 'type_year_name'
   
   data_miner do
     tap "Brighter Planet's sanitized automobile type year data", Earth.taps_server
-    
-    process "pull dependencies" do
-      run_data_miner_on_belongs_to_associations
-    end
-  end
-  
-  # AutomobileFuel needs this to calculate weighted average hfc emission factor
-  def total_travel
-    automobile_type_fuel_years.map(&:total_travel).sum
-  end
-  
-  def hfc_emission_factor
-    hfc_emissions / automobile_type_fuel_years.map(&:fuel_consumption).sum
-  end
-  
-  def hfc_emission_factor_units
-    "kilograms_co2e_per_litre"
   end
 end
