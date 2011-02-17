41550f5d0de14cda5c01df3a91116cd4891467be
Ian Hough <ian@brighterplanet.com>
2011-02-16 17:03:35 -0800

    Key AutomobileFuel on name

--- lib/earth/automobile/automobile_fuel/data_miner.rb
+++ lib/earth/automobile/automobile_fuel/data_miner.rb
@@ -1,8 +1,8 @@
 AutomobileFuel.class_eval do
   data_miner do
     schema Earth.database_options do
-      string 'code'
       string 'name'
+      string 'code'
       string 'base_fuel_name'
       string 'blend_fuel_name'
       float  'blend_portion' # the portion of the blend that is the blend fuel
@@ -12,8 +12,8 @@ AutomobileFuel.class_eval do
     
     import "a list of pure automobile fuels",
            :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdE9xTEdueFM2R0diNTgxUlk1QXFSb2c&output=csv' do
-      key   'code'
-      store 'name'
+      key   'name'
+      store 'code'
       store 'base_fuel_name'
       store 'distance_key'
       store 'ef_key'
@@ -21,14 +21,16 @@ AutomobileFuel.class_eval do
     
     import "a list of blended automobile fuels",
            :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdEswNGIxM0U4U0N1UUppdWw2ejJEX0E&output=csv' do
-      key   'code'
-      store 'name'
+      key   'name'
+      store 'code'
       store 'base_fuel_name'
       store 'blend_fuel_name'
       store 'blend_portion'
       store 'distance_key'
       store 'ef_key'
     end
+    
+    # FIXME TODO verify code somehow
     
     %w{ base_fuel_name distance_key ef_key }.each do |attribute|
       verify "#{attribute.humanize} should never be missing" do
