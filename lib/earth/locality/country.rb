253215c0f914bc8bab31f64acdbfd3d7cecdd644
Ian Hough <ian@brighterplanet.com>
2011-03-07 15:35:02 -0800

    Add global and US-specific automobile trip data to Country

--- lib/earth/locality/country.rb
+++ lib/earth/locality/country.rb
@@ -1,7 +1,17 @@
 class Country < ActiveRecord::Base
   set_primary_key :iso_3166_code
   
-  falls_back_on :flight_route_inefficiency_factor => 1.10 # for international flights - this is the larger (European) factor from Kettunen et al. (2005) http://www.atmseminar.org/seminarContent/seminar6/papers/p_055_MPM.pdf
+  falls_back_on :name => 'fallback',
+                :automobile_urbanity => lambda { Country.united_states.automobile_urbanity }, # for now assume US represents world
+                :automobile_fuel_efficiency => ((22.5 + 16.2) / 2.0).miles_per_gallon.to(:kilometres_per_litre), # average of passenger car fuel unknown and light goods vehicle fuel unknown - WRI Mobile Combustion calculation tool v2.0
+                :automobile_fuel_efficiency_units => 'kilometres_per_litre',
+                :automobile_city_speed => lambda { Country.united_states.automobile_city_speed }, # for now assume US represents world
+                :automobile_city_speed_units => lambda { Country.united_states.automobile_city_speed_units }, # for now assume US represents world
+                :automobile_highway_speed => lambda { Country.united_states.automobile_highway_speed }, # for now assume US represents world
+                :automobile_highway_speed_units => lambda { Country.united_states.automobile_highway_speed_units }, # for now assume US represents world
+                :automobile_trip_distance => lambda { Country.united_states.automobile_trip_distance }, # for now assume US represents world
+                :automobile_trip_distance_units => lambda { Country.united_states.automobile_trip_distance_units }, # for now assume US represents world
+                :flight_route_inefficiency_factor => lambda { Country.maximum(:flight_route_inefficiency_factor) } # default to the largest inefficiency factor
   
   data_miner do
     tap "Brighter Planet's sanitized countries list", Earth.taps_server
--- lib/earth/locality/country/data_miner.rb
+++ lib/earth/locality/country/data_miner.rb
@@ -3,6 +3,15 @@ Country.class_eval do
     schema Earth.database_options do
       string 'iso_3166_code'
       string 'name'
+      float  'automobile_urbanity'
+      float  'automobile_fuel_efficiency'
+      string 'automobile_fuel_efficiency_units'
+      float  'automobile_city_speed'
+      string 'automobile_city_speed_units'
+      float  'automobile_highway_speed'
+      string 'automobile_highway_speed_units'
+      float  'automobile_trip_distance'
+      string 'automobile_trip_distance_units'
       float  'flight_route_inefficiency_factor'
     end
     
@@ -21,5 +30,62 @@ Country.class_eval do
       key   'iso_3166_code'
       store 'flight_route_inefficiency_factor'
     end
+    
+    import "automobile trip fallbacks",
+           :url => 'https://spreadsheets.google.com/pub?hl=en&hl=en&key=0AoQJbWqPrREqdDdZRm1tNjY0c2dYNG00bXJ3TXRqUVE&gid=0&output=csv' do
+      key 'iso_3166_code'
+      store 'automobile_urbanity'
+      store 'automobile_city_speed', :units_field_name => 'automobile_city_speed_units'
+      store 'automobile_highway_speed', :units_field_name => 'automobile_highway_speed_units'
+      store 'automobile_trip_distance', :units_field_name => 'automobile_trip_distance_units'
+    end
+    
+    # FIXME TODO eventually need to do this for all countries
+    process "Derive US average automobile fuel efficiency from AutomobileTypeFuelYear" do
+      # AutomobileTypeFuelYear.run_data_miner!
+      
+      scope = AutomobileTypeFuelYear.where(:year => AutomobileTypeFuelYear.maximum(:year))
+      fe = scope.sum(:total_travel) / scope.sum(:fuel_consumption)
+      units = scope.first.total_travel_units + '_per_' + scope.first.fuel_consumption_units.singularize
+      
+      connection.execute %{
+        UPDATE countries
+        SET automobile_fuel_efficiency = #{fe},
+            automobile_fuel_efficiency_units = '#{units}'
+        WHERE iso_3166_code = 'US'
+      }
+    end
+    
+    process "Convert automobile city speed from miles per hour to kilometres per hour" do
+      conversion_factor = 1.miles.to(:kilometres)
+      connection.execute %{
+        UPDATE countries
+        SET automobile_city_speed = automobile_city_speed * #{conversion_factor},
+            automobile_city_speed_units = 'kilometres_per_hour'
+        WHERE automobile_city_speed_units = 'miles_per_hour'
+      }
+    end
+    
+    process "Convert automobile highway speed from miles per hour to kilometres per hour" do
+      conversion_factor = 1.miles.to(:kilometres)
+      connection.execute %{
+        UPDATE countries
+        SET automobile_highway_speed = automobile_highway_speed * #{conversion_factor},
+            automobile_highway_speed_units = 'kilometres_per_hour'
+        WHERE automobile_highway_speed_units = 'miles_per_hour'
+      }
+    end
+    
+    process "Convert automobile trip distance from miles to kilometres" do
+      conversion_factor = 1.miles.to(:kilometres)
+      connection.execute %{
+        UPDATE countries
+        SET automobile_trip_distance = automobile_trip_distance * #{conversion_factor},
+            automobile_trip_distance_units = 'kilometres'
+        WHERE automobile_trip_distance_units = 'miles'
+      }
+    end
+    
+    # FIXME TODO verify this
   end
 end
