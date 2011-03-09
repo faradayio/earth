bc0a79d879aa4e282e2f6cba6b2620bfbffc3127
Ian Hough <ian@brighterplanet.com>
2011-03-09 15:52:28 -0800

    @WIP use global data for Fuel

--- lib/earth/fuel/fuel/data_miner.rb
+++ lib/earth/fuel/fuel/data_miner.rb
@@ -1,9 +1,21 @@
 Fuel.class_eval do
+  class IpccFuelParser
+    def initialize(options = {})
+      # nothing
+    end
+    
+    def apply(row)
+      # FIXME TODO
+      # This should make the changes shown in https://spreadsheets.google.com/ccc?key=0AoQJbWqPrREqdDZ1dS1vYU5aQkJpQWt4UW0yby1XTFE&hl=en#gid=0
+    end
+  end
+  
   data_miner do
     schema Earth.database_options do
       string 'name'
       float  'density'
       string 'density_units'
+      string 'calorific_basis'
       float  'energy_content'
       string 'energy_content_units'
       float  'carbon_content'
@@ -16,16 +28,11 @@ Fuel.class_eval do
       string 'co2_biogenic_emission_factor_units'
     end
     
-    process "Derive fuel names from FuelYear" do
-      FuelYear.run_data_miner!
-      INSERT_IGNORE %{INTO fuels(name)
-        SELECT DISTINCT fuel_years.fuel_name FROM fuel_years
-      }
-    end
-    
-    import "fuels with non-variable characteristics",
-           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdGJmYkdtajZyV3Byb0lrd21xLVhXUGc&hl=en&gid=0&output=csv' do
+    import "global fuel data derived from the IPCC Guidelines for National Greenhouse Gas Inventories",
+           :url => 'https://spreadsheets.google.com/pub?hl=en&hl=en&key=0AoQJbWqPrREqdFk5dzJvT2ozNjdEU05pWVJUTUpTOXc&gid=0&output=csv' do
+           # :transform => { :class => IpccFuelParser } do
       key 'name'
+      store 'calorific_basis'
       store 'energy_content', :units_field_name => 'energy_content_units'
       store 'carbon_content', :units_field_name => 'carbon_content_units'
       store 'oxidation_factor'
@@ -39,19 +46,13 @@ Fuel.class_eval do
     end
     
     process "Calculate CO2 and CO2 biogenic emission factors" do
-      conversion_factor = (1.0 / 1_000.0) * (44.0 / 12.0) # Google: 1 kg / 1e3 g * 44 CO2 / 12 C
+      conversion_factor = (1_000.0 / 1.0) * (1.0 / 1_000_000.0) * (44.0 / 12.0) # Google: 1000 Gg / 1 Tg * 1 Gg / 1_000_000 kg * 44 CO2 / 12 C
       connection.execute %{
         UPDATE fuels
         SET co2_emission_factor = carbon_content * energy_content * oxidation_factor * (1 - biogenic_fraction) * #{conversion_factor},
-            co2_biogenic_emission_factor = carbon_content * energy_content * oxidation_factor * biogenic_fraction * #{conversion_factor}
-      }
-      conversion_factor = (1_000.0 / 1.0) * (1.0 / 1_000_000.0) * (44.0 / 12.0) # Google: 1000 Gg / 1 Tg * 1 Gg / 1_000_000 kg * 44 CO2 / 12 C
-      
-      Fuel.all.each do |record|
-        record.co2_emission_factor = record.carbon_content * record.energy_content * record.oxidation_factor * (1 - record.biogenic_fraction) * conversion_factor
-        record.co2_emission_factor_units = ""
-        
-        co2_biogenic_emission_factor = carbon_content * energy_content * oxidation_factor * biogenic_fraction * #{conversion_factor}
+            co2_biogenic_emission_factor = carbon_content * energy_content * oxidation_factor * biogenic_fraction * #{conversion_factor},
+            co2_emission_factor_units = 'kilograms_per_kilogram',
+            co2_biogenic_emission_factor_units = 'kilograms_per_kilogram'
       }
     end
     
