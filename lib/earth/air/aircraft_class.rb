79c9afc7bce92a0f10a40dfc1fb67d076736016d
Ian Hough <ian@brighterplanet.com>
2011-04-28 17:16:09 -0700

    Update AircraftClass

--- lib/earth/air/aircraft_class.rb
+++ lib/earth/air/aircraft_class.rb
@@ -1,8 +1,6 @@
 class AircraftClass < ActiveRecord::Base
   set_primary_key :code
   
-  # has_many :aircraft, :foreign_key => 'class_code', :primary_key => 'code'
-  
   data_miner do
     tap "Brighter Planet's aircraft class data", Earth.taps_server
   end
--- lib/earth/air/aircraft_class/data_miner.rb
+++ lib/earth/air/aircraft_class/data_miner.rb
@@ -3,29 +3,39 @@ AircraftClass.class_eval do
     schema Earth.database_options do
       string  'code'
       string  'name'
-      float   'm1'
-      float   'm2'
       float   'm3'
-      float   'endpoint_fuel'
+      string  'm3_units'
+      float   'm2'
+      string  'm2_units'
+      float   'm1'
+      string  'm1_units'
+      float   'b'
+      string  'b_units'
       float   'seats'
     end
     
     import "a list of Brighter Planet-defined aircraft classes",
-           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdGNBbHFibmxJUFprQkUwZHp6VU51Smc&hl=en&gid=0&output=csv' do
+           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdGNBbHFibmxJUFprQkUwZHp6VU51Smc&output=csv' do
       key 'code', :field_name => 'aircraft_class_code'
       store 'name'
     end
     
-    process "Derive some average aircraft chraracteristics from aircraft" do
-      Aircraft.run_data_miner!
-      aircraft = Aircraft.arel_table
-      aircraft_classes = AircraftClass.arel_table
-      conditional_relation = aircraft_classes[:code].eq(aircraft[:class_code])
+    process "Derive some average aircraft chraracteristics from aircraft and fuel use equation" do
+      AircraftClass.all.each do |aircraft_class|
+        relevant_aircraft = Aircraft.where(:class_code => aircraft_class.code)
+        
+        AircraftFuelUseEquation.weighted_average_relation(:m3, :weighted_by => [:aircraft, :passengers])
+      end
       
-      %w{ m1 m2 m3 endpoint_fuel seats }.each do |column|
-        relation = Aircraft.weighted_average_relation(column).where(conditional_relation)
+      %w{ m3 m2 m1 b seats }.each do |column|
+        relation = AircraftFuelUseEquation.weighted_average_relation(column).where(conditional_relation)
         update_all "#{column} = (#{relation.to_sql})"
       end
+      
+      update_all "m3_units = 'kilograms_per_cubic_nautical_mile'"
+      update_all "m2_units = 'kilograms_per_square_nautical_mile'"
+      update_all "m1_units = 'kilograms_per_nautical_mile'"
+      update_all "b_units  = 'kilograms'"
     end
     
     # FIXME TODO verify this
