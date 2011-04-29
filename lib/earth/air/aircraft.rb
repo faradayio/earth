ffb6114c24d0cc21535f0773165f90d9682baf84
Ian Hough <ian@brighterplanet.com>
2011-04-28 17:16:56 -0700

    Update Aircraft

--- lib/earth/air/aircraft.rb
+++ lib/earth/air/aircraft.rb
@@ -1,16 +1,52 @@
 class Aircraft < ActiveRecord::Base
   set_primary_key :icao_code
   
-  # Find every aircraft whose name is similar to search_name
-  def self.similar(search_name)
-    @ltd ||= LooseTightDictionary.new(all, :haystack_reader => lambda { |record| record.descriptive_name })
-    @ltd.find_all search_name
+  belongs_to :aircraft_class, :foreign_key => 'class_code', :primary_key => 'code'
+  
+  # associate each aircraft with aircraft similar to itself via fuzzy matches
+  # FIXME TODO is this necessary?
+  has_many :fuzzy_aircraft_matches, :foreign_key => 'icao_code', :primary_key => 'icao_code'
+  has_many :aircraft, :through => :fuzzy_aircraft_matches, :foreign_key => 'icao_code', :primary_key => 'icao_code'
+  
+  # for use in fuzzy matching - concatenate manufacturer name and model name
+  def description
+    manufacturer_name + " " + model_name
   end
   
-  belongs_to :aircraft_class,    :foreign_key => 'class_code',    :primary_key => 'code'
+  def self.ltd
+    @ltd ||= LooseTightDictionary.new(Aircraft.all, :haystack_reader => lambda { |record| record.description.downcase },
+                                                    :blockings => RemoteTable.new(:url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdDlRR2NmdzE2ZjZwTy1ucjh4cWFYOFE&gid=0&output=csv').map { |record| record['blocking'] },
+                                                    :identities => RemoteTable.new(:url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdDlRR2NmdzE2ZjZwTy1ucjh4cWFYOFE&gid=1&output=csv').map { |record| record['identity'] },
+                                                    :tighteners => RemoteTable.new(:url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdDlRR2NmdzE2ZjZwTy1ucjh4cWFYOFE&gid=2&output=csv').map { |record| record['tightener'] },
+                                                    :must_match_blocking => true,
+                                                    :first_blocking_decides => true)
+  end
+  
+  # find every aircraft whose name is similar to search_name
+  def self.fuzzy_matches(search_description)
+    ltd.find_all search_description.downcase
+  end
+  
+  # blocking (when :must_match_blocking => true)
+  # takes all blockings that match the needle
+  # sifts through haystack, and only keeps straws that match those blockings
+  # if the blocking includes captures then the match of the blocking on the straw must be the same as the match of the blocking on the needle
+  # e.g. /(boeing \d{3})/i needle = "Boeing 747" straw1 = "Boeing 727" (rejected) straw2 = "Boeing 747-100" (kept)
+  
+  # identity
+  # /(boeing).*(7\d)(0|7)/ only keeps straws whose match result of the identity is the same as the needle's match result of the identity
+  # if identity doesn't match needle, it's ignored
+  # if it matches needle but doesn't match straw, that straw is filtered out
+  # if it matches needle and straw, if the match result of both is the same keep the straw; otherwise filter it out
+  
+  # tightening
+  # /(douglas).*(md).*(\d\d)/ compares the match result, rather than the raw needle and straw
+  # does this for all tightenings, and figures out which combination has the highest possible score
+  # basically use this to get some things to have a better score than others
+  # works on our pre-screened list of straws (that we get after running blockings and identities)
   
   data_miner do
-    tap "Brighter Planet's aircraft data", Earth.taps_server
+    tap "Brighter Planet's sanitized aircraft data", Earth.taps_server
     
     process "pull dependencies" do
       run_data_miner_on_belongs_to_associations
--- lib/earth/air/aircraft/data_miner.rb
+++ lib/earth/air/aircraft/data_miner.rb
@@ -1,70 +1,11 @@
 Aircraft.class_eval do
-  # def self.bts_name_dictionary
-  #   @_bts_dictionary ||= LooseTightDictionary.new RemoteTable.new(:url => 'http://www.transtats.bts.gov/Download_Lookup.asp?Lookup=L_AIRCRAFT_TYPE', :select => lambda { |record| record['Code'].to_i.between?(1, 998) }),
-  #                                                 :tightenings  => RemoteTable.new(:url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=0&output=csv', :headers => false),
-  #                                                 :identities   => RemoteTable.new(:url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=3&output=csv', :headers => false),
-  #                                                 :blockings    => RemoteTable.new(:url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=4&output=csv', :headers => false),
-  #                                                 :blocking_only => true,
-  #                                                 :right_reader => lambda { |record| record['Description'] }
-  # end
-  # 
-  # # warning: self-referential, assumes it will be used once first import step is done
-  # def self.icao_name_dictionary
-  #   @_icao_dictionary ||= LooseTightDictionary.new Aircraft.all,
-  #                                                  :tightenings  => RemoteTable.new(:url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=0&output=csv', :headers => false),
-  #                                                  :identities   => RemoteTable.new(:url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=3&output=csv', :headers => false),
-  #                                                  :blockings    => RemoteTable.new(:url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=4&output=csv', :headers => false),
-  #                                                  :right_reader => lambda { |record| record.manufacturer_name.to_s + ' ' + record.name.to_s }
-  # end
-  # 
-  # class Aircraft::BtsMatcher
-  #   attr_reader :wants
-  #   def initialize(wants)
-  #     @wants = wants
-  #   end
-  #   def match(raw_faa_icao_record)
-  #     @_match ||= Hash.new
-  #     return @_match[raw_faa_icao_record] if @_match.has_key?(raw_faa_icao_record)
-  #     faa_icao_record = [ raw_faa_icao_record['Manufacturer'] + ' ' + raw_faa_icao_record['Model'] ]
-  #     bts_record = Aircraft.bts_name_dictionary.left_to_right faa_icao_record
-  #     retval = case wants
-  #     when :bts_aircraft_type_code
-  #       bts_record['Code']
-  #     when :bts_name
-  #       bts_record['Description']
-  #     end if bts_record
-  #     @_match[raw_faa_icao_record] = retval
-  #   end
-  # end
-  # 
-  # class Aircraft::FuelUseMatcher
-  #   def match(raw_fuel_use_record)
-  #     @_match ||= Hash.new
-  #     return @_match[raw_fuel_use_record] if @_match.has_key?(raw_fuel_use_record)
-  #     
-  #     aircraft_record = if raw_fuel_use_record['ICAO'] =~ /\A[0-9A-Z]+\z/
-  #       Aircraft.find_by_icao_code raw_fuel_use_record['ICAO']
-  #     end
-  #     
-  #     aircraft_record ||= if raw_fuel_use_record['Aircraft Name'].present?
-  #       Aircraft.icao_name_dictionary.left_to_right [ raw_fuel_use_record['Aircraft Name'] ]
-  #     end
-  #     
-  #     if aircraft_record
-  #       @_match[raw_fuel_use_record] = aircraft_record.icao_code
-  #     else
-  #       raise "Didn't find a match for #{raw_fuel_use_record['Aircraft Name']} (#{raw_fuel_use_record['ICAO']}), which we found in the fuel use spreadsheet"
-  #     end
-  #   end
-  # end
-  
   # For errata
   class Aircraft::Guru
     def method_missing(method_id, *args, &block)
       if method_id.to_s =~ /\A([a-z]+)_is_n?o?t?_?([^\?]+)/
         column_name = $1
         value = $2
-        value_regexp = Regexp.new("^" + value.gsub('_', ' ') + "$", Regexp::IGNORECASE)
+        value_regexp = Regexp.new('^' + value.gsub('_', ' ') + '$', Regexp::IGNORECASE)
         matches = value_regexp.match(args.first[column_name.titleize]) # row['Manufacturer'] =~ /mcdonnell douglas/i
         method_id.to_s.include?('_not_') ? matches.nil? : !matches.nil?
       else
@@ -101,6 +42,7 @@ Aircraft.class_eval do
     end
   end
   
+  # We're only interested in aircraft from certain manufacturers
   def self.manufacturer_whitelist?(candidate)
     @manufacturer_whitelist ||= RemoteTable.new(:url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdFRFalpOdlg1cnF6amlSM1dDc1lya2c&output=csv')
     @manufacturer_whitelist.any? { |record| record['Manufacturer'].to_regexp.match(candidate) }
@@ -110,8 +52,8 @@ Aircraft.class_eval do
     schema Earth.database_options do
       string 'icao_code'
       string 'manufacturer_name'
-      string 'name'
-      string 'type'
+      string 'model_name'
+      string 'aircraft_type'
       string 'engine_type'
       float  'engines'
       string 'weight_class'
@@ -127,25 +69,29 @@ Aircraft.class_eval do
              :encoding => 'windows-1252',
              :row_xpath => '//table/tr[2]/td/table/tr',
              :column_xpath => 'td',
-             :errata => { :url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdF96ZWRuSzlsSm5PSlZ0XzlwRGRuemc&output=csv',
+             :errata => { :url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdGVBRnhkRGhSaVptSDJ5bXJGbkpUSWc&output=csv',
                           :responder => Aircraft::Guru.new },
              :select => lambda { |record| manufacturer_whitelist? record['Manufacturer'] } do
         key 'icao_code',           :field_name => 'Designator'
         store 'manufacturer_name', :field_name => 'Manufacturer'
-        store 'name',              :field_name => 'Model'
-        # store 'type',              :field_name => 'Type/Wt Class', :chars => 0
-        # store 'engine_type',       :field_name => 'Type/Wt Class', :chars => 2
-        # store 'engines',           :field_name => 'Type/Wt Class', :chars => 1
-        # store 'weight_class',      :field_name => 'Type/Wt Class', :split => { :pattern => %r{/}, :keep => 1 }
+        store 'model_name',        :field_name => 'Model'
+        store 'aircraft_type',     :field_name => 'Type/Wt Class', :chars => 0, :dictionary => {:input => 'code', :output => 'description', :url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdDJtU0stS0duR1NhQVVIU29valYwREE&output=csv'}
+        store 'engine_type',       :field_name => 'Type/Wt Class', :chars => 2, :dictionary => {:input => 'code', :output => 'description', :url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdHgtS0xRTERpbjAxSzBBNFc4LUJEMXc&output=csv'}
+        store 'engines',           :field_name => 'Type/Wt Class', :chars => 1
+        store 'weight_class',      :field_name => 'Type/Wt Class', :split => { :pattern => %r{/}, :keep => 1 }, :dictionary => {:input => 'code', :output => 'description', :url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdGhrWElDZ25oV2NPREg0eUhjRVRYUHc&output=csv'}
       end
     end
     
-    # import "aircraft not included in the FAA database",
-    #        :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdHRNaVpSUWw2Z2VhN3RUV25yYWdQX2c&output=csv' do
-    #   key 'icao_code'
-    #   store 'manufacturer_name'
-    #   store 'name'
-    # end
+    import "aircraft not included in the FAA database",
+           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdHRNaVpSUWw2Z2VhN3RUV25yYWdQX2c&output=csv' do
+      key 'icao_code'
+      store 'manufacturer_name'
+      store 'model_name'
+      store 'aircraft_type'
+      store 'engine_type'
+      store 'engines'
+      store 'weight_class'
+    end
     
     # process "Derive some average aircraft characteristics from flight segments" do
     #   FlightSegment.run_data_miner!
