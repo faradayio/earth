c5cc1b7f618f364301ebbf3e008aa6cf5a648b94
Ian Hough <ian@brighterplanet.com>
2011-05-03 16:37:16 -0700

    Clean up AircraftManufacturer

--- lib/earth/air/aircraft_manufacturer.rb
+++ lib/earth/air/aircraft_manufacturer.rb
@@ -1,8 +1,6 @@
 class AircraftManufacturer < ActiveRecord::Base
   set_primary_key :name
   
-  # has_many :aircraft, :foreign_key => 'name', :primary_key => 'bp_code'
-  
   data_miner do
     tap "Brighter Planet's aircraft manufacturer data", Earth.taps_server
   end
