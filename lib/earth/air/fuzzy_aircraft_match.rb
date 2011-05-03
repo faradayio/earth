3e9dc56c4cc77469dbc5530a2f4ccea60ba68941
Ian Hough <ian@brighterplanet.com>
2011-05-03 16:41:53 -0700

    Deal with aircraft descriptions that reference multiple variants

--- lib/earth/air/fuzzy_aircraft_match.rb
+++ lib/earth/air/fuzzy_aircraft_match.rb
@@ -1,13 +1,35 @@
 class FuzzyAircraftMatch < ActiveRecord::Base
-  belongs_to :aircraft,          :foreign_key => 'icao_code',   :primary_key => 'icao_code'
-  belongs_to :flight_segment,    :foreign_key => 'description', :primary_key => 'aircraft_description'
-  belongs_to :fuel_use_equation, :foreign_key => 'description', :primary_key => 'aircraft_description', :class_name => 'AircraftFuelUseEquation'
+  belongs_to :aircraft, :foreign_key => 'icao_code',            :primary_key => 'icao_code'
+  has_many :flight_segments, :foreign_key => 'aircraft_description', :primary_key => 'search_description'
   
   class << self
-    # create records linking the icao codes of all aircraft whose description is similar to seach_description with search_description
+    # Create records linking a search descriptoin with the icao codes of all aircraft whose description is similar to seach_description
     def populate!(search_description)
-      Aircraft.fuzzy_matches(search_description).each do |aircraft|
-        find_or_create_by_icao_code_and_description_and_search_description(aircraft.icao_code, aircraft.description, search_description)
+      # If the search description describes multiple variants, split it into a base and several suffixes
+      # e.g. 'boeing 747-100/200/300' => ['boeing 747-', '100', '200', '300']
+      if /\//.match(search_description)
+        base_and_first_suffix = search_description.split('/')[0]
+        base_length = base_and_first_suffix.rindex(/[ \-a-z]/i)
+        base = base_and_first_suffix.slice(0..base_length)
+        suffixes = search_description.split(base)[1].split('/')
+        
+        # Combine each suffix with the base to get a list of complete model ids
+        # e.g. ['boeing 747-100', 'boeing 747-200', 'boeing 747-300']
+        synthetic_descriptions = suffixes.map { |suffix| base + suffix }
+        
+        # Find the fuzzy matches for every complete model id and associate them all with the original search description
+        # e.g. 'boeing 747-100/200/300' => [B741, B742, B743]
+        synthetic_descriptions.each do |synthetic_description|
+          Aircraft.fuzzy_matches(synthetic_description).each do |aircraft|
+            FuzzyAircraftMatch.find_or_create_by_icao_code_and_description_and_search_description(aircraft.icao_code, aircraft.description, search_description)
+          end
+        end
+      # If the search description describes a single variant, find and remember the fuzzy matches
+      # e.g. 'boeing 747-400' => ['B744', 'B74D']
+      else
+        Aircraft.fuzzy_matches(search_description).each do |aircraft|
+          FuzzyAircraftMatch.find_or_create_by_icao_code_and_description_and_search_description(aircraft.icao_code, aircraft.description, search_description)
+        end
       end
     end
   end
