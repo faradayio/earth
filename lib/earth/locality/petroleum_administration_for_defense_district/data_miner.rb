30ab58dddbf6ef43ed7b2779d5e6888fe61f5e20
Ian Hough <ian@brighterplanet.com>
2011-06-22 13:09:34 -0400

    Nullify blanks in the petroleum_districts table

--- lib/earth/locality/petroleum_administration_for_defense_district/data_miner.rb
+++ lib/earth/locality/petroleum_administration_for_defense_district/data_miner.rb
@@ -3,11 +3,10 @@ PetroleumAdministrationForDefenseDistrict.class_eval do
     import "a list of PADD districts and states derived from the EIA PADD definitions",
            :url => 'http://spreadsheets.google.com/pub?key=t5HM1KbaRngmTUbntg8JwPA&gid=0&output=csv' do
       key   'code',             :field_name => 'Code'
-      store 'district_code',    :field_name => 'PAD district code'
-      store 'subdistrict_code', :field_name => 'PAD subdistrict code'
-      store 'district_name',    :field_name => 'PAD district name'
-      store 'subdistrict_name', :field_name => 'PAD subdistrict name'
+      store 'district_code',    :field_name => 'PAD district code',    :nullify => true
+      store 'subdistrict_code', :field_name => 'PAD subdistrict code', :nullify => true
+      store 'district_name',    :field_name => 'PAD district name',    :nullify => true
+      store 'subdistrict_name', :field_name => 'PAD subdistrict name', :nullify => true
     end
   end
 end
-
