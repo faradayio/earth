2dcf2acf7428949143dbff155c3487625184ef90
Seamus Abshere <seamus@abshere.net>
2011-06-24 13:24:28 -0500

    fix LodgingClass schema damage from 782d08fbde

--- lib/earth/hospitality/lodging_class.rb
+++ lib/earth/hospitality/lodging_class.rb
@@ -4,17 +4,12 @@ class LodgingClass < ActiveRecord::Base
   create_table do
     string 'name'
     float  'electricity_intensity'
-    string 'electricity_intensity'
-    float  'natural_gas_intensity'
-    string 'natural_gas_intensity'
-    float  'fuel_oil_intensity'
-    string 'fuel_oil_intensity'
-    float  'district_heat_intensity'
-    string 'district_heat_intensity'
-
-    string 'natural_gas_intensity_units'
-    string 'fuel_oil_intensity_units'
     string 'electricity_intensity_units'
+    float  'natural_gas_intensity'
+    string 'natural_gas_intensity_units'
+    float  'fuel_oil_intensity'
+    string 'fuel_oil_intensity_units'
+    float  'district_heat_intensity'
     string 'district_heat_intensity_units'
   end
 end
