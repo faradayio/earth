4ded9ce3ce1485e1051da4b313b61e18d760229d
Ian <ian@worklap.local>
2011-08-11 17:52:06 +0200

    Update our curated airlines list to mesh with a new source for airline names and codes

--- lib/earth/air/airline.rb
+++ lib/earth/air/airline.rb
@@ -1,6 +1,6 @@
 class Airline < ActiveRecord::Base
   set_primary_key :name
-
+  
   force_schema do
     string 'name'
     string 'bts_code'
--- lib/earth/air/airline/data_miner.rb
+++ lib/earth/air/airline/data_miner.rb
@@ -1,10 +1,10 @@
 Airline.class_eval do
   data_miner do
-    import "a Brighter Planet-curated list of airlines",
-           :url => 'https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdDhoVHZmSTlZcHBsRUtPR0dPd0prMkE&output=csv' do
+    import "a Brighter Planet-curated list of airlines and codes not included in our other sources",
+           :url => 'https://spreadsheets.google.com/spreadsheet/pub?key=0AoQJbWqPrREqdGJoaFpENXRqMEM2NW42am5tNURGU2c&output=csv' do
       key 'name'
       store 'bts_code',  :nullify => true
-      store 'iata_code',  :nullify => true
+      store 'iata_code', :nullify => true
       store 'icao_code', :nullify => true
     end
   end
