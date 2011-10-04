e7145cefe7780bc7e90d485e5151efe0530dde6e
ian <ian@brighterplanet.com>
2011-10-05 01:32:10 +0200

    Name the FlightSeatClass fallback for clarity

--- lib/earth/air/flight_seat_class.rb
+++ lib/earth/air/flight_seat_class.rb
@@ -3,7 +3,8 @@ class FlightSeatClass < ActiveRecord::Base
   
   has_many :distance_class_seat_classes, :foreign_key => 'seat_class_name', :primary_key => 'name', :class_name => 'FlightDistanceClassSeatClass'
   
-  falls_back_on :multiplier => 1
+  falls_back_on :name => 'fallback',
+                :multiplier => 1
   
   col :name
   col :multiplier, :type => :float
