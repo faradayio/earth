7d5dba55d5d6d6a2ec3d77de4d84ed00acfb7dde
Seamus Abshere <seamus@abshere.net>
2011-10-07 11:27:19 -0500

    standardize weighted averaging

--- lib/earth/air/flight_seat_class/data_miner.rb
+++ lib/earth/air/flight_seat_class/data_miner.rb
@@ -5,41 +5,16 @@ FlightSeatClass.class_eval do
     end
     
     process "Derive flight seat classes names from FlightDistanceClassSeatClass" do
-      FlightDistanceClassSeatClass.select("DISTINCT seat_class_name").each do |distance_class_seat_class|
-        FlightSeatClass.find_or_create_by_name(distance_class_seat_class.seat_class_name)
+      connection.select_values("SELECT DISTINCT seat_class_name FROM #{FlightDistanceClassSeatClass.quoted_table_name}").each do |seat_class_name|
+        find_or_create_by_name seat_class_name
       end
     end
     
     process "Calculate multipliers from FlightDistanceClassSeatClass and FlightDistanceClass" do
-      FlightSeatClass.find_each do |seat_class|
-        seat_class.multiplier = 
-          seat_class.distance_class_seat_classes.map do |dcsc|
-            dcsc.multiplier * dcsc.distance_class.passengers
-          end.sum / seat_class.distance_class_seat_classes.map(&:distance_class).map(&:passengers).sum
+      find_each do |seat_class|
+        seat_class.multiplier = seat_class.distance_class_seat_classes.weighted_average(:multiplier, :weighted_by => [:distance_class, :passengers])
         seat_class.save!
       end
     end
-    
-    # FIXME TODO verify this
-    
-    # sabshere 5/21/10 in case we ever need this
-    # class << self
-    #   def refresh
-    #     update_all_weighted_averages(:seats,      :weighted_by => :airline_aircraft_seat_classes)
-    #     update_all_weighted_averages(:multiplier, :weighted_by => :airline_aircraft_seat_classes)
-    #   end
-    # 
-    #   def safe_find_by_name(name)
-    #     guess = case name.to_s.downcase.gsub(/[^a-z]/, '')
-    #     when /first/
-    #       'first'
-    #     when /busi/, /exec/
-    #       'business'
-    #     when /econ/, /coach/
-    #       'economy'
-    #     end
-    #     find_or_create_by_name(guess) unless guess.nil?
-    #   end
-    # end
   end
 end
