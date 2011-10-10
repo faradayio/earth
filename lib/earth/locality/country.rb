82d4a301f58012d38ca66a93a23703a41f5c2f94
Ian Hough <ian@brighterplanet.com>
2011-10-10 17:17:11 +0200

    DRY

--- lib/earth/locality/country.rb
+++ lib/earth/locality/country.rb
@@ -4,16 +4,16 @@ class Country < ActiveRecord::Base
   has_many :rail_companies, :foreign_key => 'country_iso_3166_code'
   
   falls_back_on :name => 'fallback',
-                :automobile_urbanity => lambda { Country.united_states.automobile_urbanity }, # for now assume US represents world
+                :automobile_urbanity => lambda { united_states.automobile_urbanity }, # for now assume US represents world
                 :automobile_fuel_efficiency => ((22.5 + 16.2) / 2.0).miles_per_gallon.to(:kilometres_per_litre), # average of passenger car fuel unknown and light goods vehicle fuel unknown - WRI Mobile Combustion calculation tool v2.0
                 :automobile_fuel_efficiency_units => 'kilometres_per_litre',
-                :automobile_city_speed => lambda { Country.united_states.automobile_city_speed }, # for now assume US represents world
-                :automobile_city_speed_units => lambda { Country.united_states.automobile_city_speed_units }, # for now assume US represents world
-                :automobile_highway_speed => lambda { Country.united_states.automobile_highway_speed }, # for now assume US represents world
-                :automobile_highway_speed_units => lambda { Country.united_states.automobile_highway_speed_units }, # for now assume US represents world
-                :automobile_trip_distance => lambda { Country.united_states.automobile_trip_distance }, # for now assume US represents world
-                :automobile_trip_distance_units => lambda { Country.united_states.automobile_trip_distance_units }, # for now assume US represents world
-                :flight_route_inefficiency_factor => lambda { Country.maximum(:flight_route_inefficiency_factor) }, # default to the largest inefficiency factor
+                :automobile_city_speed => lambda { united_states.automobile_city_speed }, # for now assume US represents world
+                :automobile_city_speed_units => lambda { united_states.automobile_city_speed_units }, # for now assume US represents world
+                :automobile_highway_speed => lambda { united_states.automobile_highway_speed }, # for now assume US represents world
+                :automobile_highway_speed_units => lambda { united_states.automobile_highway_speed_units }, # for now assume US represents world
+                :automobile_trip_distance => lambda { united_states.automobile_trip_distance }, # for now assume US represents world
+                :automobile_trip_distance_units => lambda { united_states.automobile_trip_distance_units }, # for now assume US represents world
+                :flight_route_inefficiency_factor => lambda { maximum(:flight_route_inefficiency_factor) }, # default to the largest inefficiency factor
                 :rail_trip_distance => lambda { weighted_average(:rail_trip_distance, :weighted_by => :rail_passengers) },
                 :rail_trip_distance_units => 'kilometres', # FIXME TODO derive this
                 :rail_speed => lambda { weighted_average(:rail_speed, :weighted_by => :rail_passengers) },
