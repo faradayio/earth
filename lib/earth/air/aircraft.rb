591854dc352fd1ee31e2ef5f9236a902f85d5683
Ian Hough <ijhough@gmail.com>
2011-11-02 18:41:43 +0100

    Fix Aircraft.fallback fuel use coefficients

--- lib/earth/air/aircraft.rb
+++ lib/earth/air/aircraft.rb
@@ -111,10 +111,10 @@ class Aircraft < ActiveRecord::Base
     [m3, m2, m1, b].all?(&:present?) and [m3, m2, m1, b].any?(&:nonzero?)
   end
   
-  falls_back_on :m3 => lambda { weighted_average(:m3, :weighted_by => [:aircraft, :passengers]) }, # 9.73423082858437e-08   r7110: 8.6540464368905e-8      r6972: 8.37e-8
-                :m2 => lambda { weighted_average(:m2, :weighted_by => [:aircraft, :passengers]) }, # -0.000134350543484608  r7110: -0.00015337661447817    r6972: -4.09e-5
-                :m1 => lambda { weighted_average(:m1, :weighted_by => [:aircraft, :passengers]) }, # 6.7728101555467        r7110: 4.7781966869412         r6972: 7.85
-                :b  => lambda { weighted_average(:b,  :weighted_by => [:aircraft, :passengers]) }, # 1527.81790006167       r7110: 1065.3476555284         r6972: 1.72e3
+  falls_back_on :m3 => lambda { weighted_average(:m3, :weighted_by => :passengers) }, # 9.73423082858437e-08   r7110: 8.6540464368905e-8      r6972: 8.37e-8
+                :m2 => lambda { weighted_average(:m2, :weighted_by => :passengers) }, # -0.000134350543484608  r7110: -0.00015337661447817    r6972: -4.09e-5
+                :m1 => lambda { weighted_average(:m1, :weighted_by => :passengers) }, # 6.7728101555467        r7110: 4.7781966869412         r6972: 7.85
+                :b  => lambda { weighted_average(:b,  :weighted_by => :passengers) }, # 1527.81790006167       r7110: 1065.3476555284         r6972: 1.72e3
                 :m3_units => 'kilograms_per_cubic_nautical_mile',
                 :m2_units => 'kilograms_per_square_nautical_mile',
                 :m1_units => 'kilograms_per_nautical_mile',
