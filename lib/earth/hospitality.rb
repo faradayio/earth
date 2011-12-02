4c90ab42516a974185b3c8fd41454eb45ec6af2c
Ian Hough <ijhough@gmail.com>
2011-12-02 18:18:26 +0100

    Add CensusRegionLodgingClass to hold census region-specific lodging class fuel intensities

--- lib/earth/hospitality.rb
+++ lib/earth/hospitality.rb
@@ -1,2 +1,3 @@
+require 'earth/hospitality/census_region_lodging_class'
 require 'earth/hospitality/country_lodging_class'
 require 'earth/hospitality/lodging_class'
--- lib/earth/hospitality/data_miner.rb
+++ lib/earth/hospitality/data_miner.rb
@@ -1,2 +1,3 @@
+require 'earth/hospitality/census_region_lodging_class/data_miner'
 require 'earth/hospitality/country_lodging_class/data_miner'
 require 'earth/hospitality/lodging_class/data_miner'
