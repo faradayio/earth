5a180797353a68e33f155517022f66fdba0a837a
Derek Kastner <dkastner@gmail.com>
2011-12-02 17:01:43 -0500

    Add NAICS import to Industry

--- lib/earth/industry/industry.rb
+++ lib/earth/industry/industry.rb
@@ -11,6 +11,17 @@ class Industry < ActiveRecord::Base
   
   col :naics_code
   col :description, :type => :text
+  
+  data_miner do
+    import "the U.S. Census 2002 NAICS code list",
+           :url => 'http://www.census.gov/epcd/naics02/naicod02.txt',
+           :skip => 4,
+           :headers => false,
+           :delimiter => '	' do
+      key 'naics_code', :field_number => 0
+      store 'description', :field_number => 1
+    end
+  end
 
   def trade_industry?
     prefix = naics_code.to_s[0,2]
