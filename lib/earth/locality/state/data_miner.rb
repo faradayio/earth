db31e94f1d5154e1afab041cc65a430803f38bb5
Ian Hough <ijhough@gmail.com>
2012-01-06 12:55:30 -0500

    Fix state import so it doesn't import US territories or add extra rows

--- lib/earth/locality/state/data_miner.rb
+++ lib/earth/locality/state/data_miner.rb
@@ -1,21 +1,22 @@
 State.class_eval do
   data_miner do
-    # state names, ansi codes, and postal abbreviations
+    # state names, FIPS codes, and postal abbreviations
     import 'the U.S. Census State ANSI Code file',
            :url => 'http://www.census.gov/geo/www/ansi/state.txt',
-           :delimiter => '|' do
+           :delimiter => '|',
+           :select => lambda { |record| record['STATE'].to_i < 60 } do
       key   'postal_abbreviation', :field_name => 'STUSAB'
-      store 'fips_code', :field_name => 'STATE'
-      store 'name', :field_name => 'STATE_NAME'
+      store 'fips_code',           :field_name => 'STATE'
+      store 'name',                :field_name => 'STATE_NAME'
     end
-
+    
     # census divisions
-    import('the U.S. Census Geographic Terms and Definitions',
+    import 'state census divisions from the U.S. Census',
            :url => 'http://www.census.gov/popest/about/geo/state_geocodes_v2009.txt',
-           :skip => 6,
-           :headers => %w{ Region Division FIPS Name },
-           :select => ::Proc.new { |row| row['FIPS'].to_i > 0 }) do
-      key   'fips_code', :field_name => 'FIPS CODE STATE'
+           :skip => 8,
+           :headers => ['Region', 'Division', 'State FIPS', 'Name'],
+           :select => ::Proc.new { |row| row['State FIPS'].to_i > 0 } do
+      key   'fips_code', :field_name => 'State FIPS'
       store 'census_division_number', :field_name => 'Division'
     end
     
@@ -27,4 +28,3 @@ State.class_eval do
     end
   end
 end
-
