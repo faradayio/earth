7cd8d5feb3f1698d3ed58d21f2f8ca09108a58d4
Ian Hough <ijhough@gmail.com>
2012-01-23 23:48:27 +0100

    Add a method to look up climate zone number to ClimateDivision

--- lib/earth/locality/climate_division.rb
+++ lib/earth/locality/climate_division.rb
@@ -5,7 +5,23 @@ class ClimateDivision < ActiveRecord::Base
   belongs_to :state, :foreign_key => 'state_postal_abbreviation'
   
   RADIUS = 750
-
+  
+  def climate_zone_number
+    if cooling_degree_days < 2000
+      if heating_degree_days > 7000
+        1
+      elsif heating_degree_days > 5499
+        2
+      elsif heating_degree_days > 3999
+        3
+      else
+        4
+      end
+    else
+      5
+    end
+  end
+  
   col :name
   col :heating_degree_days, :type => :float
   col :cooling_degree_days, :type => :float
