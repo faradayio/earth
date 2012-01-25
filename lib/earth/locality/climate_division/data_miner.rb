f8019f826f5161291ca443f71b9b10ff2976299e
Ian Hough <ijhough@gmail.com>
2012-01-25 18:02:22 +0100

    Typo

--- lib/earth/locality/climate_division/data_miner.rb
+++ lib/earth/locality/climate_division/data_miner.rb
@@ -3,8 +3,8 @@ ClimateDivision.class_eval do
     import "a list of climate divisions and their average heating and cooling degree days",
            :url => 'http://static.brighterplanet.com/science/data/climate/climate_divisions/climate_divisions.csv' do
       key 'name'
-      store 'heating_degree_days', :from_units => :degrees_fahrenheit, :to_units => :degress_celsius
-      store 'cooling_degree_days', :from_units => :degrees_fahrenheit, :to_units => :degress_celsius
+      store 'heating_degree_days', :from_units => :degrees_fahrenheit, :to_units => :degrees_celsius
+      store 'cooling_degree_days', :from_units => :degrees_fahrenheit, :to_units => :degrees_celsius
       store 'state_postal_abbreviation'
     end
   end
