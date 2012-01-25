55beae4ebcad41efd6920c2554ecfac058981c5a
Ian Hough <ijhough@gmail.com>
2012-01-25 16:58:22 +0100

    Optionally run tests using mysql's test_earth database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,13 +8,36 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'earth'
 
+case ENV['EARTH_DB_ADAPTER']
+when 'mysql'
+  adapter = 'mysql'
+  database = 'test_earth'
+  username = 'root'
+  password = 'password'
+  
+  # system %{mysql -u #{username} -p#{password} -e "DROP DATABASE #{database}"}
+  # system %{mysql -u #{username} -p#{password} -e "CREATE DATABASE #{database}"}
+else
+  adapter = 'sqlite3'
+  database = ':memory:'
+  username = nil
+  password = nil
+end
+
+config = {
+  'encoding' => 'utf8',
+  'adapter' => adapter,
+  'database' => database,
+}
+config['username'] = username if username
+config['password'] = password if password
+
+ActiveRecord::Base.establish_connection config
+
 logger = Logger.new 'log/test.log'
 logger.level = Logger::DEBUG
 
-ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => ':memory:'
-
 ActiveRecord::Base.logger = logger
-
 DataMiner.logger = logger
 
 RSpec.configure do |c|
