b86b22955fa18fb42faed12dc1ecc719d932c23f
Derek Kastner <dkastner@gmail.com>
2012-02-10 14:32:50 -0500

    More detailed spot_check failures

--- spec/support/integration.rb
+++ spec/support/integration.rb
@@ -7,8 +7,10 @@ module Integration
         where_clause[field] = test[0][i]
       end
       record = records.where(where_clause).first
+      record.should_not be_nil, "Expected to find a record with #{where_clause.inspect}"
       headers[1].each_with_index do |field, i|
-        record.send(field).should send("be_#{test[1][i]}")
+        criterion = test[1][i]
+        record.send(field).should send("be_#{criterion}"), "Expecting #{field} to be #{criterion} on #{record.inspect}"
       end
     end
   end
