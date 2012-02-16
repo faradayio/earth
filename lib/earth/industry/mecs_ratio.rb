35ef210295b34a9486e023aa508007a98d8fd2c3
Ian Hough <ijhough@gmail.com>
2012-02-16 17:04:25 +0100

    Add intelligent lookup by naics code to MecsRatio

--- lib/earth/industry/mecs_ratio.rb
+++ lib/earth/industry/mecs_ratio.rb
@@ -12,14 +12,20 @@ class MecsRatio < ActiveRecord::Base
   col :energy_per_dollar_of_shipments, :type => :float
   col :energy_per_dollar_of_shipments_units
   
-  # Find the first MecsRatio whose census_region_number matches number and whose naics_code matches code.
+  # Find the first record whose naics_code matches code.
+  # If no record found chop off the last character of code and try again, and so on.
+  def self.find_by_naics_code(code)
+    find_by_naics_code_and_census_region_number(code, nil)
+  end
+  
+  # Find the first record whose census_region_number matches number and whose naics_code matches code.
   # If none found, chop off the last character of code and try again, and so on.
   def self.find_by_naics_code_and_census_region_number(code, number)
     if code.blank?
       record = nil
     else
       code = Industry.format_naics_code code
-      record = where('census_region_number = ? AND naics_code = ?', number, code).first
+      record = where(:census_region_number => number, :naics_code => code).first
       record ||= find_by_naics_code_and_census_region_number(code[0..-2], number)
     end
     record
--- spec/earth/industry/mecs_ratio_spec.rb
+++ spec/earth/industry/mecs_ratio_spec.rb
@@ -7,34 +7,36 @@ describe MecsRatio do
   end
   
   describe '.find_by_naics_code_and_census_region_number' do
-    before do
-      MecsRatio.create! :naics_code => '311', :census_region_number => 1, :energy_per_dollar_of_shipments => 11
-      MecsRatio.create! :naics_code => '3112', :census_region_number => 1, :energy_per_dollar_of_shipments => 12
-      MecsRatio.create! :naics_code => '311221', :census_region_number => 1, :energy_per_dollar_of_shipments => 13
-      MecsRatio.create! :naics_code => '31131', :census_region_number => 1, :energy_per_dollar_of_shipments => 13
-      MecsRatio.create! :naics_code => '311', :census_region_number => 2, :energy_per_dollar_of_shipments => 17
-      MecsRatio.create! :naics_code => '3112', :census_region_number => 2, :energy_per_dollar_of_shipments => 18
-      MecsRatio.create! :naics_code => '311221', :census_region_number => 2, :energy_per_dollar_of_shipments => 19
-      MecsRatio.create! :naics_code => '31131', :census_region_number => 2, :energy_per_dollar_of_shipments => 20
-    end
-    
     it 'finds an exact match' do
-      ratio = MecsRatio.find_by_naics_code_and_census_region_number('311221', 2)
-      ratio.naics_code.should == '311221'
-      ratio.census_region_number.should == 2
+      MecsRatio.find_by_naics_code_and_census_region_number('311221', 2).
+        name.should == '311221-2'
     end
     it 'finds a parent category when exact code is not present' do
-      ratio = MecsRatio.find_by_naics_code_and_census_region_number('3117', 2)
-      ratio.naics_code.should == '311'
-      ratio.census_region_number.should == 2
+      MecsRatio.find_by_naics_code_and_census_region_number('3117', 2).
+        name.should == '311-2'
     end
     it 'finds a parent category rather than a sibling category' do
-      ratio = MecsRatio.find_by_naics_code_and_census_region_number('311225', 2)
-      ratio.naics_code.should == '3112'
-      ratio.census_region_number.should == 2
+      MecsRatio.find_by_naics_code_and_census_region_number('311225', 2).
+        name.should == '3112-2'
     end
     it 'returns nil if no match found' do
-      MecsRatio.find_by_naics_code_and_census_region_number('543211', 2).should be_nil
+      MecsRatio.find_by_naics_code_and_census_region_number('543211', 2).
+        should be_nil
+    end
+  end
+  
+  describe '.find_by_naics_code' do
+    it 'finds an exact match' do
+      MecsRatio.find_by_naics_code('311221').name.should == '311221-'
+    end
+    it 'finds a parent category when exact code is not present' do
+      MecsRatio.find_by_naics_code('3117').name.should == '311-'
+    end
+    it 'finds a parent category rather than a sibling category' do
+      MecsRatio.find_by_naics_code('311225').name.should == '3112-'
+    end
+    it 'returns nil if no match found' do
+      MecsRatio.find_by_naics_code('543211').should be_nil
     end
   end
 end
