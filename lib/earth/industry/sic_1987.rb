2114259b470aff7a9065065c69c8ae8d3854e1e5
Ian Hough <ijhough@gmail.com>
2012-03-06 17:42:00 +0100

    Don't try to be fancy

--- lib/earth/industry/sic_1987.rb
+++ lib/earth/industry/sic_1987.rb
@@ -4,7 +4,6 @@ class Sic1987 < ActiveRecord::Base
   
   has_many :naics_2002_sic_1987_concordances, :foreign_key => :sic_1987_code
   has_many :naics_2002, :through => :naics_2002_sic_1987_concordances
-  has_many :industries, :through => :naics_2002
   
   # for data import
   def self.format_description(description)
