945c95a39431d1f136a7f1fb0fa535bc662dd442
Ian Hough <ijhough@gmail.com>
2012-05-10 08:55:15 +0200

    Import 2012 FEG

--- spec/earth/automobile/automobile_make_model_year_variant_spec.rb
+++ spec/earth/automobile/automobile_make_model_year_variant_spec.rb
@@ -9,7 +9,7 @@ describe AutomobileMakeModelYearVariant do
     
     it 'should import data' do
       AutomobileMakeModelYearVariant.run_data_miner!
-      AutomobileMakeModelYearVariant.all.count.should == 27638
+      AutomobileMakeModelYearVariant.all.count.should == 28769
     end
   end
   
@@ -40,12 +40,155 @@ describe AutomobileMakeModelYearVariant do
       AutomobileMakeModelYearVariant.where(:year => 2007).count.should == 1176
       AutomobileMakeModelYearVariant.where(:year => 2008).count.should == 1239
       AutomobileMakeModelYearVariant.where(:year => 2009).count.should == 1180
-      AutomobileMakeModelYearVariant.where(:year => 2010).count.should == 1105
+      AutomobileMakeModelYearVariant.where(:year => 2010).count.should == 1107
       AutomobileMakeModelYearVariant.where(:year => 2011).count.should == 1097
+      AutomobileMakeModelYearVariant.where(:year => 2012).count.should == 1129
     end
     
-    # fuel type code should be in AutomobileFuel
-    # fuel efficiencies should be > 0
-    # fuel efficiencies should be km / l
+    it 'should have correct 2010 data' do
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Acura').count.should == 11
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Aston Martin').count.should == 7
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Audi').count.should == 35
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Bentley').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'BMW').count.should == 83
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Bugatti').count.should == 1
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Buick').count.should == 9
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Cadillac').count.should == 27
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Chevrolet').count.should == 88
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Chrysler').count.should == 16
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Dodge').count.should == 39
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Ferrari').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Ford').count.should == 56
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'GMC').count.should == 50
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Honda').count.should == 31
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Hummer').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Hyundai').count.should == 31
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Infiniti').count.should == 19
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Jaguar').count.should == 9
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Jeep').count.should == 27
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Kia').count.should == 26
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Lamborghini').count.should == 9
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Land Rover').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Lexus').count.should == 23
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Lincoln').count.should == 12
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Lotus').count.should == 3
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Maserati').count.should == 5
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Maybach').count.should == 3
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Mazda').count.should == 27
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Mercedes-Benz').count.should == 47
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Mercury').count.should == 18
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Mini').count.should == 15
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Mitsubishi').count.should == 23
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Nissan').count.should == 51
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Pontiac').count.should == 24
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Porsche').count.should == 47
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Rolls-Royce').count.should == 5
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Roush Performance').count.should == 2
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Saab').count.should == 13
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Saturn').count.should == 16
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Scion').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Smart').count.should == 2
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Spyker').count.should == 1
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Subaru').count.should == 20
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Suzuki').count.should == 26
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Toyota').count.should == 57
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Volkswagen').count.should == 34
+      AutomobileMakeModelYearVariant.where(:year => 2010, :make_name => 'Volvo').count.should == 29
+    end
+    
+    it 'should have correct 2011 data' do
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Acura').count.should == 8
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Aston Martin').count.should == 8
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Audi').count.should == 38
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Bentley').count.should == 5
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'BMW').count.should == 83
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Bugatti').count.should == 1
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Buick').count.should == 10
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Cadillac').count.should == 29
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Chevrolet').count.should == 90
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Chrysler').count.should == 10
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Dodge').count.should == 36
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Ferrari').count.should == 9
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Ford').count.should == 79
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'GMC').count.should == 62
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Honda').count.should == 31
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Hyundai').count.should == 34
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Infiniti').count.should == 18
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Jaguar').count.should == 8
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Jeep').count.should == 22
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Kia').count.should == 35
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Lamborghini').count.should == 4
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Land Rover').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Lexus').count.should == 24
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Lincoln').count.should == 14
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Mahindra').count.should == 1
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Maserati').count.should == 5
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Mazda').count.should == 27
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Mercedes-Benz').count.should == 53
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Mercury').count.should == 13
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Mini').count.should == 21
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Mitsubishi').count.should == 27
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Nissan').count.should == 53
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Porsche').count.should == 54
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Rolls-Royce').count.should == 5
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Saab').count.should == 15
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Scion').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Smart').count.should == 4
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Subaru').count.should == 19
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Suzuki').count.should == 23
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Toyota').count.should == 59
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Volkswagen').count.should == 28
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'Volvo').count.should == 18
+      AutomobileMakeModelYearVariant.where(:year => 2011, :make_name => 'VPG').count.should == 2
+    end
+    
+    it 'should have correct 2012 data' do
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Acura').count.should == 12
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Aston Martin').count.should == 11
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Audi').count.should == 39
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Azure Dynamics').count.should == 2
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Bentley').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'BMW').count.should == 82
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Bugatti').count.should == 1
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Buick').count.should == 14
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Cadillac').count.should == 19
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Chevrolet').count.should == 95
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Chrysler').count.should == 12
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'CODA Automotive').count.should == 1
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Dodge').count.should == 31
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Fiat').count.should == 4
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Ford').count.should == 77
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'GMC').count.should == 65
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Honda').count.should == 28
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Hyundai').count.should == 35
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Infiniti').count.should == 22
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Jaguar').count.should == 10
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Jeep').count.should == 25
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Kia').count.should == 37
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Lamborghini').count.should == 5
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Land Rover').count.should == 7
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Lexus').count.should == 20
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Lincoln').count.should == 12
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Lotus').count.should == 4
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Maserati').count.should == 3
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Maybach').count.should == 5
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Mazda').count.should == 24
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Mercedes-Benz').count.should == 61
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Mini').count.should == 31
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Mitsubishi').count.should == 24
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Nissan').count.should == 53
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Porsche').count.should == 67
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Rolls-Royce').count.should == 6
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Roush Performance').count.should == 2
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Saab').count.should == 16
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Scion').count.should == 7
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Smart').count.should == 2
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Subaru').count.should == 19
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Suzuki').count.should == 21
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Toyota').count.should == 57
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Volkswagen').count.should == 39
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'Volvo').count.should == 15
+      AutomobileMakeModelYearVariant.where(:year => 2012, :make_name => 'VPG').count.should == 1
+    end
   end
 end
