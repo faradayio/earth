e4484a88632d176e89d41fb2e69faca4918c32f2
Seamus Abshere <seamus@abshere.net>
2012-05-10 15:24:08 -0500

    (unfortunate?) convention

--- lib/earth/electricity/electric_market.rb
+++ lib/earth/electricity/electric_market.rb
@@ -4,9 +4,9 @@ class ElectricMarket < ActiveRecord::Base
   self.primary_key = "id" # string
   
   belongs_to :electric_utility, :foreign_key => 'electric_utility_eia_id'
-  belongs_to :zip_code, :foreign_key => 'zip_code'
+  belongs_to :zip_code, :foreign_key => 'zip_code_name'
   
   col :id
   col :electric_utility_eia_id, :type => :integer
-  col :zip_code
+  col :zip_code_name
 end
--- lib/earth/electricity/electric_market/data_miner.rb
+++ lib/earth/electricity/electric_market/data_miner.rb
@@ -2,12 +2,12 @@ ElectricMarket.class_eval do
   data_miner do
     import 'IOU markets', :url => 'http://en.openei.org/datasets/files/899/pub/iou_zipcodes.csv' do
       key 'id', :synthesize => proc { |row| row['compid'] + row['zip'] }
-      store 'zip_code', :field_name => 'zip'
+      store 'zip_code_name', :field_name => 'zip'
       store 'electric_utility_eia_id', :field_name => 'compid'
     end
     import 'Non-IOU markets', :url => 'http://en.openei.org/datasets/files/899/pub/non-iou_zipcodes.csv' do
       key 'id', :synthesize => proc { |row| row['compid'] + row['zip'] }
-      store 'zip_code', :field_name => 'zip'
+      store 'zip_code_name', :field_name => 'zip'
       store 'electric_utility_eia_id', :field_name => 'compid'
     end
   end
