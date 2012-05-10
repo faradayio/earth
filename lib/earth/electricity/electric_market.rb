6f908bf619f1b3a0700dfd0fcf017b18c163deaa
Seamus Abshere <seamus@abshere.net>
2012-05-10 14:55:27 -0500

    poor man's composite key

--- lib/earth/electricity/electric_market.rb
+++ lib/earth/electricity/electric_market.rb
@@ -1,11 +1,12 @@
 require 'earth/locality'
 
 class ElectricMarket < ActiveRecord::Base
-  self.primary_key = "FIXME" # dunno what this should be
+  self.primary_key = "id" # string
   
   belongs_to :electric_utility, :foreign_key => 'electric_utility_eia_id'
   belongs_to :zip_code, :foreign_key => 'zip_code'
   
+  col :id
   col :electric_utility_eia_id, :type => :integer
   col :zip_code
 end
--- lib/earth/electricity/electric_market/data_miner.rb
+++ lib/earth/electricity/electric_market/data_miner.rb
@@ -1,12 +1,12 @@
 ElectricMarket.class_eval do
   data_miner do
     import 'IOU markets', :url => 'http://en.openei.org/datasets/files/899/pub/iou_zipcodes.csv' do
-      key 'FIXME' # dunno how to do this
+      key 'id', :synthesize => proc { |row| row['compid'] + row['zip'] }
       store 'zip_code', :field_name => 'zip'
       store 'electric_utility_eia_id', :field_name => 'compid'
     end
     import 'Non-IOU markets', :url => 'http://en.openei.org/datasets/files/899/pub/non-iou_zipcodes.csv' do
-      key 'FIXME' # dunno how to do this
+      key 'id', :synthesize => proc { |row| row['compid'] + row['zip'] }
       store 'zip_code', :field_name => 'zip'
       store 'electric_utility_eia_id', :field_name => 'compid'
     end
