0373b9896258ad2a9d8f070c4f03be5df1f2ee1f
Seamus Abshere <seamus@abshere.net>
2012-05-10 15:29:44 -0500

    typo

--- lib/earth/electricity/electric_market/data_miner.rb
+++ lib/earth/electricity/electric_market/data_miner.rb
@@ -2,14 +2,14 @@ ElectricMarket.class_eval do
   data_miner do
     import 'IOU markets',
            :url => 'http://en.openei.org/datasets/files/899/pub/iou_zipcodes.csv',
-           :select => proc { |row| row['compid'] =~ /^\d+$/ do
+           :select => proc { |row| row['compid'] =~ /^\d+$/ } do
       key 'id', :synthesize => proc { |row| row['compid'] + row['zip'] }
       store 'zip_code_name', :field_name => 'zip'
       store 'electric_utility_eia_id', :field_name => 'compid'
     end
     import 'Non-IOU markets',
            :url => 'http://en.openei.org/datasets/files/899/pub/non-iou_zipcodes.csv',
-           :select => proc { |row| row['compid'] =~ /^\d+$/ do
+           :select => proc { |row| row['compid'] =~ /^\d+$/ } do
       key 'id', :synthesize => proc { |row| row['compid'] + row['zip'] }
       store 'zip_code_name', :field_name => 'zip'
       store 'electric_utility_eia_id', :field_name => 'compid'
