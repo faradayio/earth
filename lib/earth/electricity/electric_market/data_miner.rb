61f28e861e79422e6af76f102fd69bbfeb604e78
Ian Hough <ijhough@gmail.com>
2012-05-11 15:52:53 +0200

    Errata for ElectricMarket data

--- lib/earth/electricity/electric_market/data_miner.rb
+++ lib/earth/electricity/electric_market/data_miner.rb
@@ -4,15 +4,16 @@ ElectricMarket.class_eval do
   data_miner do
     import 'IOU markets',
            :url => 'http://en.openei.org/datasets/files/899/pub/iou_zipcodes.csv',
-           :select => proc { |row| row['compid'] =~ /^\d+$/ } do
-      key 'id', :synthesize => proc { |row| row['compid'] + row['zip'] }
+           :errata => { :url => "file://#{Earth::ERRATA_DIR}/electricity/electric_market/nrel_errata.csv" } do
+      key 'id', :synthesize => proc { |row| row['compid'].to_i.to_s + '-' + row['zip'] }
       store 'zip_code_name', :field_name => 'zip'
       store 'electric_utility_eia_id', :field_name => 'compid'
     end
+    
     import 'Non-IOU markets',
            :url => 'http://en.openei.org/datasets/files/899/pub/non-iou_zipcodes.csv',
-           :select => proc { |row| row['compid'] =~ /^\d+$/ } do
-      key 'id', :synthesize => proc { |row| row['compid'] + row['zip'] }
+           :errata => { :url => "file://#{Earth::ERRATA_DIR}/electricity/electric_market/nrel_errata.csv" } do
+      key 'id', :synthesize => proc { |row| row['compid'].to_i.to_s + '-' + row['zip'] }
       store 'zip_code_name', :field_name => 'zip'
       store 'electric_utility_eia_id', :field_name => 'compid'
     end
--- spec/earth/electricity/electric_market_spec.rb
+++ spec/earth/electricity/electric_market_spec.rb
@@ -1,21 +1,25 @@
 require 'spec_helper'
-require 'earth/electricity/electric_utility'
+require 'earth/electricity/electric_market'
+require 'earth/electricity/electric_utility' # needed for test verifying link with ElectricUtility
 
-describe ElectricUtility do
-  describe 'Electric market data', :data_miner => true do
+describe ElectricMarket do
+  describe 'import', :data_miner => true do
     before :all do
-      Earth.init :electricity, :load_data_miner => true
-      ElectricMarket.run_data_miner!
-    end
-
-    it 'Properly links 53704 to MG&E' do
-      ZipCode.find('53704').electric_utilities.should_include ElectricUtility.find(11479)
+      Earth.init :electricity, :load_data_miner => true, :skip_parent_associations => true
     end
     
-    after :all do
-      ZipCode.delete_all
-      ElectricMarket.delete_all
-      ElectricUtility.delete_all
+    it 'should import data' do
+      ElectricMarket.run_data_miner!
+    end
+  end
+  
+  describe 'verify imported data', :sanity => true do
+    it { ElectricMarket.count.should == 64864 }
+    
+    it { ElectricMarket.where(:electric_utility_eia_id => nil).count.should == 0 } # some ids aren't found in our electric_utilities table
+    
+    it 'Properly links 53704 to MG&E' do
+      ZipCode.find('53704').electric_utilities.should include(ElectricUtility.find 11479)
     end
   end
 end
