9932d0ee85a8e466e9909c513482d34f2afa0dfb
Ian Hough <ijhough@gmail.com>
2012-05-23 18:06:35 +0200

    Add weighting based on EPA data to AutomobileYear

--- lib/earth/automobile/automobile_year/data_miner.rb
+++ lib/earth/automobile/automobile_year/data_miner.rb
@@ -5,7 +5,7 @@ AutomobileYear.class_eval do
       AutomobileMakeModelYearVariant.run_data_miner!
     end
     
-    process "Derive year names from automobile make model year variants" do
+    process "Derive year names from AutomobileMakeModelYearVariant" do
       ::Earth::Utils.insert_ignore(
         :src => AutomobileMakeModelYearVariant,
         :dest => AutomobileYear,
