4458de041e121e74301d1e92fcc5e5ab59d0eebb
Ian Hough <ijhough@gmail.com>
2012-05-23 18:02:51 +0200

    Update AutomobileMakeModelYear: Improve identification of hybrids Add fuel codes and alt fuel efficiencies Add weighting for calculating AutomobileMakeModel fuel efficiency

--- lib/earth/automobile/automobile_make_model_year.rb
+++ lib/earth/automobile/automobile_make_model_year.rb
@@ -5,14 +5,20 @@ class AutomobileMakeModelYear < ActiveRecord::Base
   # Need this so Automobile and AutomobileTrip can do characteristics[:make_model_year].automobile_fuel
   belongs_to :automobile_fuel, :foreign_key => 'fuel_code', :primary_key => 'code'
   
-  col :name # make + model + year
+  col :name
   col :make_name
   col :model_name
-  col :year,                    :type => :integer
+  col :year, :type => :integer
+  col :hybridity,:type => :boolean
   col :fuel_code
-  col :hybridity,               :type => :boolean
-  col :fuel_efficiency_city,    :type => :float
+  col :fuel_efficiency_city, :type => :float
   col :fuel_efficiency_city_units
   col :fuel_efficiency_highway, :type => :float
   col :fuel_efficiency_highway_units
+  col :alt_fuel_code
+  col :alt_fuel_efficiency_city, :type => :float
+  col :alt_fuel_efficiency_city_units
+  col :alt_fuel_efficiency_highway, :type => :float
+  col :alt_fuel_efficiency_highway_units
+  col :weighting, :type => :float # for calculating AutomobileMakeModel fuel efficiencies
 end
