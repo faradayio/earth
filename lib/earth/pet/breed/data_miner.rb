4520a3d00f278a9097ae9314fb1a232cf20b9f50
Seamus Abshere <seamus@abshere.net>
2012-06-07 15:18:18 -0500

    freebase.com titleized this column names

--- lib/earth/pet/breed/data_miner.rb
+++ lib/earth/pet/breed/data_miner.rb
@@ -10,7 +10,7 @@ Breed.class_eval do
     end
     
     import "a list of horse breeds", :url => 'http://www.freebase.com/type/exporttypeinstances/base/horses/horse_breed?page=0&filter_mode=type&filter_view=table&show%01p%3D%2Ftype%2Fobject%2Fname%01index=0&show%01p%3D%2Fcommon%2Ftopic%2Fimage%01index=1&show%01p%3D%2Fcommon%2Ftopic%2Farticle%01index=2&sort%01p%3D%2Ftype%2Fobject%2Ftype%01p%3Dlink%01p%3D%2Ftype%2Flink%2Ftimestamp%01index=false&=&exporttype=csv-8' do
-      key 'name'
+      key 'name', :field_name => 'Name'
       store 'species_name', :static => 'horse'
     end
   end
