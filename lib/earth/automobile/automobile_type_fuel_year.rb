2765bc2425992f059c69a52a546d44b795e2eddc
Ian Hough <ijhough@gmail.com>
2012-06-08 23:12:28 +0200

    Merge Automobile TypeFuelYearAge into TypeFuelYear

--- lib/earth/automobile/automobile_type_fuel_year.rb
+++ lib/earth/automobile/automobile_type_fuel_year.rb
@@ -1,68 +1,29 @@
-require 'earth/fuel'
 class AutomobileTypeFuelYear < ActiveRecord::Base
   self.primary_key = "name"
   
-  # FIXME TODO keep this until fix AutomobileFuel import so that it doesn't call type_fuel_year.type_year
-  belongs_to :type_year, :class_name => 'AutomobileTypeYear', :foreign_key => 'type_year_name'
+  # Used by Automobile and AutomobileTrip
+  def self.find_by_type_name_and_fuel_common_name_and_closest_year(type_name, fuel_common_name, year)
+    if year > maximum(:year)
+      where(:type_name => type_name, :fuel_common_name => fuel_common_name, :year => maximum(:year)).first
+    else
+      where(:type_name => type_name, :fuel_common_name => fuel_common_name, :year => [year, minimum(:year)].max).first
+    end
+  end
+  
+  # for calculating ch4 and n2o ef
+  def type_fuel_year_controls
+    AutomobileTypeFuelYearControl.find_all_by_type_name_and_fuel_common_name_and_closest_year(type_name, fuel_common_name, year)
+  end
   
   col :name
   col :type_name
   col :fuel_common_name
   col :year, :type => :integer
-  col :type_year_name
-  col :total_travel, :type => :float
-  col :total_travel_units
-  col :fuel_consumption, :type => :float
-  col :fuel_consumption_units
+  col :share_of_type, :type => :float
+  col :annual_distance, :type => :float
+  col :annual_distance_units
   col :ch4_emission_factor, :type => :float
   col :ch4_emission_factor_units
   col :n2o_emission_factor, :type => :float
   col :n2o_emission_factor_units
-  
-  # %w{ type_name fuel_common_name type_year_name }.each do |attribute|
-  #   verify "#{attribute.humanize} should never be missing" do
-  #     AutomobileTypeFuelYear.all.each do |record|
-  #       value = record.send(:"#{attribute}")
-  #       unless value.present?
-  #         raise "Missing #{attribute.humanize.downcase} for AutomobileTypeFuelYear '#{record.name}'"
-  #       end
-  #     end
-  #   end
-  # end
-  # 
-  # verify "Year should be from 1990 to 2008" do
-  #   AutomobileTypeFuelYear.all.each do |record|
-  #     year = record.send(:year)
-  #     unless year > 1989 and year < 2009
-  #       raise "Invalid year for AutomobileTypeFuelYear '#{record.name}': #{year} (should be from 1990 to 2008)"
-  #     end
-  #   end
-  # end
-  # 
-  # %w{ total_travel fuel_consumption ch4_emission_factor n2o_emission_factor }.each do |attribute|
-  #   verify "#{attribute.humanize} should be greater than zero" do
-  #     AutomobileTypeFuelYear.all.each do |record|
-  #       value = record.send(:"#{attribute}")
-  #       unless value > 0
-  #         raise "Invalid #{attribute.humanize.downcase} for AutomobileTypeFuelYear '#{record.name}': #{value} (should be > 0)"
-  #       end
-  #     end
-  #   end
-  # end
-  # 
-  # [["total_travel_units", "kilometres"],
-  #  ["fuel_consumption_units", "litres"],
-  #  ["ch4_emission_factor_units", "kilograms_per_litre"],
-  #  ["n2o_emission_factor_units", "kilograms_per_litre"]].each do |pair|
-  #   attribute = pair[0]
-  #   proper_units = pair[1]
-  #   verify "#{attribute.humanize} should be #{proper_units.humanize.downcase}" do
-  #     AutomobileTypeFuelYear.all.each do |record|
-  #       units = record.send(:"#{attribute}")
-  #       unless units == proper_units
-  #         raise "Invalid #{attribute.humanize.downcase} for AutomobileTypeFuelYear '#{record.name}': #{units} (should be #{proper_units})"
-  #       end
-  #     end
-  #   end
-  # end
 end
