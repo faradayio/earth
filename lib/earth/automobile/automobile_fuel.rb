d224d3c1910ecb52137864ac191db19e4a3a8dcc
Ian Hough <ijhough@gmail.com>
2012-06-08 23:14:20 +0200

    Latest AutomobileFuel

--- lib/earth/automobile/automobile_fuel.rb
+++ lib/earth/automobile/automobile_fuel.rb
@@ -3,115 +3,87 @@ require 'earth/fuel'
 class AutomobileFuel < ActiveRecord::Base
   self.primary_key = "name"
   
-  has_many :type_fuel_year_ages, :class_name => 'AutomobileTypeFuelYearAge', :foreign_key => 'fuel_common_name', :primary_key => 'distance_key'
-  has_many :type_fuel_years,     :class_name => 'AutomobileTypeFuelYear',    :foreign_key => 'fuel_common_name', :primary_key => 'ef_key'
-  belongs_to :base_fuel,         :class_name => 'Fuel',                      :foreign_key => 'base_fuel_name'
-  belongs_to :blend_fuel,        :class_name => 'Fuel',                      :foreign_key => 'blend_fuel_name'
+  # for calculating energy content and co2 efs
+  belongs_to :base_fuel,  :class_name => 'Fuel', :foreign_key => 'base_fuel_name'
+  belongs_to :blend_fuel, :class_name => 'Fuel', :foreign_key => 'blend_fuel_name'
+  
+  # for calculating gas and diesel annual distance and ch4 + n2o efs
+  has_many :type_fuels, :foreign_key => :fuel_common_name, :primary_key => :common_name, :class_name => 'AutomobileTypeFuel'
+  
+  # for fallback
+  class << self
+    def diesel
+      AutomobileFuel.find 'diesel'
+    end
     
-  warn_if_blanks_in :distance_key
-  warn_if_blanks_in :ef_key
-  warn do
-    catch :culprit do
-      find_each do |record|
-        throw :culprit, %{Records exist without base_fuel (possibly invalid key "#{record.base_fuel_name}")} unless record.base_fuel
-      end
-      false
+    def gasoline
+      AutomobileFuel.find 'gasoline'
     end
-  end
-  warn do
-    if exists?(['blend_portion IS NOT NULL AND (blend_portion < ? OR blend_portion > ?)', 0, 1])
-      "Blend portions less than 0 or greater than 1"
+    
+    def fallback_blend_portion
+      diesel.total_consumption / (diesel.total_consumption + gasoline.total_consumption)
     end
-  end
-  warn do
-    %w{co2_emission_factor co2_biogenic_emission_factor}.map do |col|
-      if exists?(["#{col} IS NULL OR #{col} < ?", 0])
-        "Records non-positive #{col}"
+    
+    def determine_fallback(method)
+      if method =~ /units/
+        gasoline.send(method)
+      else
+        (fallback_blend_portion * diesel.send(method)) + ((1 - fallback_blend_portion) * gasoline.send(method))
       end
     end
   end
   
-  class << self
-    def fallback_type_fuel_year_ages
-      AutomobileTypeFuelYearAge.where(:year => AutomobileTypeFuelYearAge.maximum('year'))
-    end
-    
-    def fallback_type_fuel_years
-      AutomobileTypeFuelYear.where(:year => AutomobileTypeFuelYear.maximum('year'))
-    end
-    
-    def fallback_type_years
-      AutomobileTypeYear.where(:year => AutomobileTypeYear.maximum('year'))
-    end
-    
-    # FIXME TODO for some reason this causes the fallbacks calculation to hang (infinite loop?) if it's defined as a fallback
-    def fallback_blend_portion
-      gas_use = fallback_type_fuel_years.where(:fuel_common_name => 'gasoline').sum('fuel_consumption').to_f
-      diesel_use = fallback_type_fuel_years.where(:fuel_common_name => 'diesel').sum('fuel_consumption').to_f
-      diesel_use / (gas_use + diesel_use)
+  # Used by Automobile and AutomobileTrip to determine whether need to convert fuel efficiency units
+  def non_liquid?
+    energy_content_units != 'megajoules_per_litre'
+  end
+  
+  # Used by Automobile and AutomobileTrip to check whether user-input fuel matches one of the vehicle's fuels
+  def same_as?(other_auto_fuel)
+    unless other_auto_fuel.nil?
+      if ['G', 'R', 'P'].include? self.code
+        ['G', 'R', 'P'].include? other_auto_fuel.code
+      else
+        self == other_auto_fuel
+      end
     end
   end
   
   falls_back_on :name => 'fallback',
-                :annual_distance => lambda { fallback_type_fuel_year_ages.weighted_average(:annual_distance, :weighted_by => :vehicles) },
-                :annual_distance_units => lambda { fallback_type_fuel_year_ages.first.annual_distance_units },
-                :energy_content => lambda {
-                  (Fuel.find_by_name('Motor Gasoline').energy_content * (1 - fallback_blend_portion)) +
-                  (Fuel.find_by_name('Distillate Fuel Oil No. 2').energy_content * fallback_blend_portion)
-                },
-                :co2_emission_factor => lambda {
-                  (Fuel.find_by_name("Motor Gasoline").co2_emission_factor * (1 - fallback_blend_portion)) +
-                  (Fuel.find_by_name("Distillate Fuel Oil No. 2").co2_emission_factor * fallback_blend_portion)
-                },
-                :co2_emission_factor_units => lambda { Fuel.find_by_name("Motor Gasoline").co2_emission_factor_units },
-                :co2_biogenic_emission_factor => lambda {
-                  (Fuel.find_by_name("Motor Gasoline").co2_biogenic_emission_factor * (1 - AutomobileFuel.fallback_blend_portion)) +
-                  (Fuel.find_by_name("Distillate Fuel Oil No. 2").co2_biogenic_emission_factor * AutomobileFuel.fallback_blend_portion)
-                },
-                :co2_biogenic_emission_factor_units => lambda { Fuel.find_by_name("Motor Gasoline").co2_biogenic_emission_factor_units },
-                :ch4_emission_factor => lambda { fallback_type_fuel_years.weighted_average(:ch4_emission_factor, :weighted_by => :total_travel) * GreenhouseGas[:ch4].global_warming_potential },
-                :ch4_emission_factor_units => lambda {
-                  prefix = fallback_type_fuel_years.first.ch4_emission_factor_units.split("_per_")[0]
-                  suffix = fallback_type_fuel_years.first.ch4_emission_factor_units.split("_per_")[1]
-                  prefix + "_co2e_per_" + suffix
-                },
-                :n2o_emission_factor => lambda {
-                  fallback_type_fuel_years.weighted_average(:n2o_emission_factor, :weighted_by => :total_travel) * GreenhouseGas[:n2o].global_warming_potential
-                },
-                :n2o_emission_factor_units => lambda {
-                  prefix = fallback_type_fuel_years.first.n2o_emission_factor_units.split("_per_")[0]
-                  suffix = fallback_type_fuel_years.first.n2o_emission_factor_units.split("_per_")[1]
-                  prefix + "_co2e_per_" + suffix
-                },
-                :hfc_emission_factor => lambda { fallback_type_years.weighted_average(:hfc_emission_factor, :weighted_by => [:type_fuel_years, :total_travel]) },
-                :hfc_emission_factor_units => lambda { fallback_type_years.first.hfc_emission_factor_units }
+                :base_fuel_name => 'Motor Gasoline',
+                :blend_fuel_name => 'Distillate Fuel Oil No. 2',
+                :blend_portion                      => proc { AutomobileFuel.fallback_blend_portion },
+                :annual_distance                    => proc { AutomobileFuel.determine_fallback 'annual_distance' },
+                :annual_distance_units              => proc { AutomobileFuel.determine_fallback 'annual_distance_units' },
+                :energy_content                     => proc { AutomobileFuel.determine_fallback 'energy_content' },
+                :energy_content_units               => proc { AutomobileFuel.determine_fallback 'energy_content_units' },
+                :co2_emission_factor                => proc { AutomobileFuel.determine_fallback 'co2_emission_factor' },
+                :co2_emission_factor_units          => proc { AutomobileFuel.determine_fallback 'co2_emission_factor_units' },
+                :co2_biogenic_emission_factor       => proc { AutomobileFuel.determine_fallback 'co2_biogenic_emission_factor' },
+                :co2_biogenic_emission_factor_units => proc { AutomobileFuel.determine_fallback 'co2_biogenic_emission_factor_units' },
+                :ch4_emission_factor                => proc { AutomobileFuel.determine_fallback 'ch4_emission_factor' },
+                :ch4_emission_factor_units          => proc { AutomobileFuel.determine_fallback 'ch4_emission_factor_units' },
+                :n2o_emission_factor                => proc { AutomobileFuel.determine_fallback 'n2o_emission_factor' },
+                :n2o_emission_factor_units          => proc { AutomobileFuel.determine_fallback 'n2o_emission_factor_units' }
   
   col :name
   col :code
+  col :common_name
   col :base_fuel_name
   col :blend_fuel_name
-  col :blend_portion,                :type => :float # the portion of the blend that is the blend fuel
-  col :distance_key                                  # used to look up annual distance from AutomobileTypeFuelYear
-  col :ef_key                                        # used to look up ch4 n2o and hfc emission factors from AutomobileTypeFuelYear
-  col :annual_distance,              :type => :float
+  col :blend_portion, :type => :float # the portion of the blend that is the blend fuel
+  col :annual_distance, :type => :float
   col :annual_distance_units
-  col :energy_content,               :type => :float
+  col :energy_content, :type => :float
   col :energy_content_units
-  col :co2_emission_factor,          :type => :float
+  col :co2_emission_factor, :type => :float
   col :co2_emission_factor_units
   col :co2_biogenic_emission_factor, :type => :float
   col :co2_biogenic_emission_factor_units
-  col :ch4_emission_factor,          :type => :float
+  col :ch4_emission_factor, :type => :float
   col :ch4_emission_factor_units
-  col :n2o_emission_factor,          :type => :float
+  col :n2o_emission_factor, :type => :float
   col :n2o_emission_factor_units
-  col :hfc_emission_factor,          :type => :float
-  col :hfc_emission_factor_units
-  col :emission_factor,              :type => :float # DEPRECATED but motorcycle needs this
-  col :emission_factor_units # FIXME TODO DEPRECATED but motorcycle needs this
-  
-  CODES = {
-    :electricity => 'El',
-    :diesel => 'D'
-  }
+  col :total_consumption, :type => :float # for calculating fallback blend_portion
+  col :total_consumption_units
 end
--- spec/earth/automobile/automobile_fuel_spec.rb
+++ spec/earth/automobile/automobile_fuel_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 require 'earth/automobile'
 
 describe AutomobileFuel do
+  let(:fallback) { AutomobileFuel.fallback }
+  
   describe 'import', :data_miner => true do
     before do
-      Earth.init :automobile, :load_data_miner => true, :skip_parent_associations => :true
+      Earth.init :automobile, :load_data_miner => true
     end
     
     it 'should import data without problems' do
@@ -13,11 +15,91 @@ describe AutomobileFuel do
   end
   
   describe 'verify imported data', :sanity => true do
-    it 'should have all the data' do
-      AutomobileFuel.count.should == 10
+    it { AutomobileFuel.count.should == 12 }
+    it { AutomobileFuel.where("annual_distance >= 0").count.should == 4 }
+    it { AutomobileFuel.where("energy_content >= 0").count.should == AutomobileFuel.count }
+    it { AutomobileFuel.where("co2_emission_factor >= 0").count.should == AutomobileFuel.count - 1 }
+    it { AutomobileFuel.where("co2_biogenic_emission_factor >= 0").count.should == AutomobileFuel.count - 1 }
+    it { AutomobileFuel.where("ch4_emission_factor >= 0").count.should == AutomobileFuel.count }
+    it { AutomobileFuel.where("n2o_emission_factor >= 0").count.should == AutomobileFuel.count }
+    
+    it { AutomobileFuel.find('gasoline').annual_distance.should be_within(0.1).of(17568.6) }
+    it { AutomobileFuel.find('gasoline').energy_content.should be_within(1e-5).of(34.6272) }
+    it { AutomobileFuel.find('gasoline').co2_emission_factor.should be_within(1e-6).of(2.34183) }
+    it { AutomobileFuel.find('gasoline').ch4_emission_factor.should be_within(1e-8).of(4.2548e-4) }
+    it { AutomobileFuel.find('gasoline').n2o_emission_factor.should be_within(1e-6).of(5.7340e-3) }
+    
+    it { AutomobileFuel.find('gasoline').annual_distance_units.should == 'kilometres' }
+    it { AutomobileFuel.find('gasoline').energy_content_units.should == 'megajoules_per_litre' }
+    it { AutomobileFuel.find('gasoline').co2_emission_factor_units.should == 'kilograms_per_litre' }
+    it { AutomobileFuel.find('gasoline').ch4_emission_factor_units.should == 'kilograms_co2e_per_kilometre' }
+    it { AutomobileFuel.find('gasoline').n2o_emission_factor_units.should == 'kilograms_co2e_per_kilometre' }
+    
+    it { AutomobileFuel.find('diesel').annual_distance.should be_within(0.1).of(22410.2) }
+    it { AutomobileFuel.find('diesel').energy_content.should be_within(1e-5).of(38.5491) }
+    it { AutomobileFuel.find('diesel').co2_emission_factor.should be_within(1e-6).of(2.70219) }
+    it { AutomobileFuel.find('diesel').ch4_emission_factor.should be_within(1e-9).of(1.3887e-5) }
+    it { AutomobileFuel.find('diesel').n2o_emission_factor.should be_within(1e-8).of(2.5812e-4) }
+    
+    # it { AutomobileFuel.find('gasoline').total_consumption.should > 455_000_000_000 }
+    # it { AutomobileFuel.find('diesel').total_consumption.should > 5_800_000_000 }
+    # 
+    it { AutomobileFuel.find('Electricity').energy_content.should == 3.6 }
+    it { AutomobileFuel.find('Electricity').co2_emission_factor.should == nil }
+    it { AutomobileFuel.find('Electricity').co2_biogenic_emission_factor.should == nil }
+    it { AutomobileFuel.find('Electricity').ch4_emission_factor.should == 0 }
+    it { AutomobileFuel.find('Electricity').n2o_emission_factor.should == 0 }
+    
+    it "all grades of gasoline should be the same" do
+      AutomobileFuel.where("name LIKE '%gasoline'").each do |fuel|
+        fuel.common_name.should == 'gasoline'
+        fuel.co2_emission_factor.should == AutomobileFuel.gasoline.co2_emission_factor
+        fuel.ch4_emission_factor.should == AutomobileFuel.gasoline.ch4_emission_factor
+        fuel.n2o_emission_factor.should == AutomobileFuel.gasoline.n2o_emission_factor
+      end
     end
-    it 'correctly assigns hfc_emission_factor' do
-      AutomobileFuel.first.hfc_emission_factor.should == 0.124799
-    end
+  end
+  
+  describe '.gasoline' do
+    it { AutomobileFuel.gasoline.should == AutomobileFuel.find('gasoline') }
+  end
+  
+  describe '.diesel' do
+    it { AutomobileFuel.diesel.should == AutomobileFuel.find('diesel') }
+  end
+  
+  describe '.fallback_blend_portion' do
+    it { AutomobileFuel.fallback_blend_portion.should be_within(1e-5).of(0.01260) }
+  end
+  
+  describe '.fallback' do
+    it { AutomobileFuel.fallback.name.should == 'fallback' }
+    it { fallback.base_fuel.should == Fuel.find('Motor Gasoline') }
+    it { fallback.blend_fuel.should == Fuel.find('Distillate Fuel Oil No. 2') }
+    it { fallback.blend_portion.should be_within(1e-5).of(0.01260) }
+    
+    it { fallback.annual_distance.should be_within(0.1).of(17629.6) }
+    it { fallback.energy_content.should be_within(1e-6).of(34.6766) }
+    it { fallback.co2_emission_factor.should be_within(1e-6).of(2.34637) }
+    it { fallback.co2_biogenic_emission_factor.should == 0.0 }
+    it { fallback.ch4_emission_factor.should be_within(1e-8).of(4.203e-4) }
+    it { fallback.n2o_emission_factor.should be_within(1e-8).of(5.665e-3) }
+    
+    it { fallback.energy_content_units.should == 'megajoules_per_litre' }
+    it { fallback.co2_emission_factor_units.should == 'kilograms_per_litre' }
+    it { fallback.co2_biogenic_emission_factor_units.should == 'kilograms_per_litre' }
+    it { fallback.ch4_emission_factor_units.should == 'kilograms_co2e_per_kilometre' }
+    it { fallback.n2o_emission_factor_units.should == 'kilograms_co2e_per_kilometre' }
+  end
+  
+  describe '#non_liquid?' do
+    it { AutomobileFuel.where(:energy_content_units => 'megajoules_per_litre').first.non_liquid?.should == false }
+    it { AutomobileFuel.where("energy_content_units != 'megajoules_per_litre'").first.non_liquid?.should == true }
+  end
+  
+  describe '#same_as?' do
+    it { AutomobileFuel.find_by_code('G').same_as?(AutomobileFuel.find_by_code 'G').should == true }
+    it { AutomobileFuel.find_by_code('G').same_as?(AutomobileFuel.find_by_code 'R').should == true }
+    it { AutomobileFuel.find_by_code('G').same_as?(AutomobileFuel.find_by_code 'D').should == false }
   end
 end
