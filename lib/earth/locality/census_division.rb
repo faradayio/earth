4a68f80ec720312beff08d93b5e3ed60c62c2fc8
Ian Hough <ijhough@gmail.com>
2012-06-14 14:32:38 +0200

    Make it easy to identify CensusDivision.fallback

--- lib/earth/locality/census_division.rb
+++ lib/earth/locality/census_division.rb
@@ -9,7 +9,8 @@ class CensusDivision < ActiveRecord::Base
   has_many :residential_energy_consumption_survey_responses, :foreign_key => 'census_division_number'
   
   # https://spreadsheets.google.com/pub?key=0AoQJbWqPrREqdFJ3U3VaSG1oQW1yclY3M3FsRzRDNFE&hl=en&output=html
-  falls_back_on :meeting_building_natural_gas_intensity => 0.0004353615 * 100.cubic_feet.to(:cubic_metres) / 1.square_feet.to(:square_metres),
+  falls_back_on :name => 'fallback',
+                :meeting_building_natural_gas_intensity => 0.0004353615 * 100.cubic_feet.to(:cubic_metres) / 1.square_feet.to(:square_metres),
                 :meeting_building_fuel_oil_intensity => 0.0000925593.gallons.to(:litres) / 1.square_feet.to(:square_metres),
                 :meeting_building_electricity_intensity => 0.0084323684 / 1.square_feet.to(:square_metres),
                 :meeting_building_district_heat_intensity => 0.0004776370.kbtus.to(:megajoules) / 1.square_feet.to(:square_metres)
