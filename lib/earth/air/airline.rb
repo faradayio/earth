ab15acd9a9a53850c4b81dca455f05dce89f389c
Derek Kastner <dkastner@gmail.com>
2012-07-11 11:29:48 -0400

    Separate keys onto separate lines

--- lib/earth/air/airline.rb
+++ lib/earth/air/airline.rb
@@ -1,7 +1,8 @@
 class Airline < ActiveRecord::Base
   self.primary_key = "name"
 
-  has_many :flight_segments, :primary_key => :bts_code,
+  has_many :flight_segments,
+    :primary_key => :bts_code,
     :foreign_key => :airline_bts_code
   
   col :name
