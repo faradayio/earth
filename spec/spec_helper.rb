9e54bfe8025edda92286d020cb924d51f2dc9ae7
Ian Hough <ijhough@gmail.com>
2012-07-27 10:15:04 +0200

    Cleanup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 ENV['EARTH_ENV'] ||= 'test'
-
 require 'earth'
 
 require 'support/integration'
@@ -16,33 +15,24 @@ include Integration
 
 Earth.logger.level = Logger::DEBUG
 
-logger = Logger.new 'log/test.log'
-ActiveRecord::Base.logger = logger
-DataMiner.logger = logger
+ActiveRecord::Base.logger = DataMiner.logger = Logger.new('log/test.log')
 
 DataMiner.unit_converter = :conversions
 
 RSpec.configure do |c|
-  unless ENV['ALL'] == 'true'
-    c.filter_run_excluding :data_miner => true
-    c.filter_run_excluding :sanity => true
-  end
+  c.filter_run_excluding(:data_miner => true) unless ENV['MINE'] == 'true'
+  c.filter_run_excluding(:sanity => true) unless ENV['SANITY'] == 'true'
+  c.filter_run_excluding(:flight_segment => true) if ENV['SKIP_FLIGHT_SEGMENT'] == 'true'
   c.before :all do
     Earth.init :all, :load_data_miner => true, :skip_parent_associations => :true
   end
-  c.before :all, :data_miner => true do
-    Earth.run_data_miner!
-  end
-  if ENV['SKIP_FLIGHT_SEGMENT'] == 'true'
-    c.filter_run_excluding :flight_segment => true
-  end
-
-  c.before(:each) do
+  
+  c.before :each do
     ActiveRecord::Base.connection.increment_open_transactions
     ActiveRecord::Base.connection.transaction_joinable = false
     ActiveRecord::Base.connection.begin_db_transaction
   end
-  c.after(:each) do
+  c.after :each do
     ActiveRecord::Base.connection.rollback_db_transaction
     ActiveRecord::Base.connection.decrement_open_transactions
   end
