35f938f76b2aa76adb7fb202cfc5d883f8845d0a
Derek Kastner <dkastner@gmail.com>
2012-08-02 14:43:30 -0500

    Use AR's built-in connection choosing, fix db tasks

--- lib/earth/tasks.rb
+++ lib/earth/tasks.rb
@@ -1,16 +1,18 @@
 require 'earth'
 require 'rake'
+require 'active_record/connection_adapters/abstract/connection_specification'
 
 module Earth
   class Tasks
     include Rake::DSL
 
-    def initialize(auto_configure = true)
-      init_earth_tasks(auto_configure)
+    def initialize
+      init_earth_tasks
       init_bare unless Object.const_defined?('Rails')
 
       namespace :db do
-        task :load_config => 'earth:db:load_config'
+        task :create => 'earth:db:create'
+        task :drop => 'earth:db:drop'
         task :migrate => 'earth:db:migrate'
         task :seed => 'earth:db:seed'
       end
@@ -23,15 +25,29 @@ module Earth
       load 'active_record/railties/databases.rake'
 
       Rake::Task['db:load_config'].clear
+      Rake::Task['db:create'].clear
+      Rake::Task['db:drop'].clear
       Rake::Task['db:migrate'].clear
       Rake::Task['db:seed'].clear
     end
+
+    def config
+      spec = ENV['DATABASE_URL']
+      resolver = ActiveRecord::Base::ConnectionSpecification::Resolver.new spec, {}
+      resolver.spec.config.stringify_keys
+    end
         
-    def init_earth_tasks(auto_configure = true)
+    def init_earth_tasks
       namespace :earth do
         namespace :db do
+          task :create do
+            create_database(config)
+          end
+          task :drop do
+            drop_database_and_rescue(config)
+          end
           task :load_config do
-            ActiveRecord::Base.configurations = Earth.database_configurations if auto_configure
+            Earth.connect
           end
           task :migrate => :load_config do
             Earth.reset_schemas!
